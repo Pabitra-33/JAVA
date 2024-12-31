ab224287a5f2f8ab797461e34539cf3f9fe3047b
Pabitra-33 <kumarpabitra345@gmail.com>
2024-12-31 16:41:54 +0530

    first commit

---
+++ Basic_Programs/src/com/SelectionStatement/If_ElseImplementation.java
@@ -0,0 +1,21 @@
+package com.SelectionStatement;
+
+//Write a java program to implement if-else block in java.
+import java.util.Scanner;
+
+public class If_ElseImplementation {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int n = scn.nextInt();
+		
+		if(n % 2 == 0)
+		{
+			System.out.println(n+" is an Even Number");
+		}
+		else {
+			System.out.println(n+" is an Odd Number");
+		}
+	}
+}
