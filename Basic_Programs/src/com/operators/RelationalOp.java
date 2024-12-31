b4b5eb8c0751810f6947a03059231b29f239254c
Pabitra-33 <kumarpabitra345@gmail.com>
2024-12-31 15:48:05 +0530

    first commit

---
+++ Basic_Programs/src/com/operators/RelationalOp.java
@@ -0,0 +1,15 @@
+package com.operators;
+
+public class RelationalOp {
+
+	public static void main(String[] args) {
+		System.out.println(10 < 20);//true
+		System.out.println(30 > 60);//false
+		System.out.println(40 >= 20);//true
+		System.out.println(10 <= 30);//true
+		System.out.println(10 > 10);//false
+		System.out.println(10 == 20);//false
+		System.out.println(10 != 10);//false
+	}
+
+}
