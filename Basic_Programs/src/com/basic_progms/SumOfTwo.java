52525173a7b5d93ed458128fbe3926d660c20d37
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-02 15:09:06 +0530

    first commit

---
+++ Basic_Programs/src/com/basic_progms/SumOfTwo.java
@@ -0,0 +1,18 @@
+package com.basic_progms;
+
+import java.util.Scanner;
+
+public class SumOfTwo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first number: ");
+		int first = sc.nextInt();
+		System.out.println("Enter second number: ");
+		int second = sc.nextInt();
+		
+		int sum = 0;//to calculate sum of two no and store it in a variable.
+		sum = first + second;
+		System.out.println("Sum of two numbers "+first+" and "+second+" is = "+sum);
+	}
+
+}
