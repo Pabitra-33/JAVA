1ee77cf720a408809d16d244ca4ec84a6541b920
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-02 16:16:30 +0530

    first commit

---
+++ Basic_Programs/src/com/basic_progms/PrintDaysOfWeek.java
@@ -0,0 +1,46 @@
+package com.basic_progms;
+
+import java.util.Scanner;
+
+//to print the days of week based on the user input.
+public class PrintDaysOfWeek {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter a number between 1-7: ");
+		int dayno = scn.nextInt();
+		
+		switch (dayno) {
+		case 1:
+			System.out.println("Sunday");
+			break;
+			
+		case 2:
+			System.out.println("Monday");
+			break;
+
+		case 3:
+			System.out.println("Tuesday");
+			break;
+			
+		case 4:
+			System.out.println("Wednesday");
+			break;
+
+		case 5:
+			System.out.println("Thursday");
+			break;
+
+		case 6:
+			System.out.println("Friday");
+			break;
+
+		case 7:
+			System.out.println("Saturday");
+			break;
+
+		default:
+			System.out.println("Invalid input");
+		}
+	}
+
+}
