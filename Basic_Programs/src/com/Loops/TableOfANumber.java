ca490bb7bd2f3ec620fbdc3357fe2afc3a5bd4ec
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-03 07:47:51 +0530

    first commit

---
+++ Basic_Programs/src/com/Loops/TableOfANumber.java
@@ -0,0 +1,18 @@
+package com.Loops;
+
+import java.util.Scanner;
+
+public class TableOfANumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int n = sc.nextInt()
+;		
+		for(int i=1; i<=10; i++)
+		{
+			System.out.println(n+" X "+i+" = "+(n*i));
+		}
+	}
+
+}
