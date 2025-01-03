bed7d314e62bf1fcbf40b0229bca3c6f7d343492
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-03 07:42:29 +0530

    first commit

--- Basic_Programs/src/com/Loops/FirstTenEvenNumbers.java
+++ Basic_Programs/src/com/Loops/FirstTenEvenNumbers.java
@@ -3,8 +3,17 @@ package com.Loops;
 public class FirstTenEvenNumbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int count = 0; // Counter to keep track of the number of even numbers printed
+        int number = 2; // Start from the first even number
 
+        System.out.println("First 10 Even Numbers are:");
+
+        // Loop until we print 10 even numbers
+        while (count < 10) {
+            System.out.println(number); // Print the current even number
+            number += 2; // Increment by 2 to get the next even number
+            count++; // Increment the counter
+        }
 	}
 
 }
