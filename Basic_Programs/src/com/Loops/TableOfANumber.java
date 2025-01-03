808ae851e6247e8640fa61dd26334aea666bdc70
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-04 00:44:42 +0530

    Printing statement added

--- Basic_Programs/src/com/Loops/TableOfANumber.java
+++ Basic_Programs/src/com/Loops/TableOfANumber.java
@@ -8,8 +8,9 @@ public class TableOfANumber {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number: ");
-		int n = sc.nextInt()
-;		
+		int n = sc.nextInt();
+		
+		System.out.println("Table of a given number "+n+" is: ");
 		for(int i=1; i<=10; i++)
 		{ 
 			System.out.println(n+" X "+i +" = "+(n*i));
