bb4f5d3c6597ab78a7c4635e9b22f6b8ce811660
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-04 01:07:11 +0530

    implemented do-while loop

--- Basic_Programs/src/com/Loops/DoWhileLoop.java
+++ Basic_Programs/src/com/Loops/DoWhileLoop.java
@@ -1,18 +1,24 @@
 package com.Loops;
 
+import java.util.Scanner;
+
 public class DoWhileLoop {
 	
 	public static void main(String[] args) 
 	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a range: ");
+		int range = sc.nextInt();
+		
 		int i = 1;
         int sum = 0;
         
         do {
             sum += i;
             i++;
-        } while (i <= 5);
+        } while (i <= range);
         
-        System.out.println("Sum of numbers from 1 to 5 is: " + sum);
+        System.out.println("Sum of numbers from 1 to "+range+" is: " + sum);
 	}
 
 }
