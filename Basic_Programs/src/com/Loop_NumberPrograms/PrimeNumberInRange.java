934d9809b4cb8162ec22a73f357b9b0b4447d41a
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-06 15:50:19 +0530

    prime number in range

---
+++ Basic_Programs/src/com/Loop_NumberPrograms/PrimeNumberInRange.java
@@ -0,0 +1,32 @@
+package com.Loop_NumberPrograms;
+
+import java.util.Scanner;
+
+public class PrimeNumberInRange {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first range: ");
+		int start = sc.nextInt();
+		System.out.println("Enter end range: ");
+		int end = sc.nextInt();
+		
+		for(int i=start; i<=end; i++) {
+			if(isPrime(i)) {
+				System.out.println(i);
+			}
+		}
+	}
+	
+	public static boolean isPrime(int n)
+	{
+		if(n <= 1) return false;
+		
+		for(int i=2; i<=n/2; i++) {
+			if(n%i == 0)
+				return false;
+		}
+		return true;
+	}
+
+}
