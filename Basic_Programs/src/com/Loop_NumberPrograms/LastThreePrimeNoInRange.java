a00896f898b2a8d90c2cb4be50a8423062262f6c
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-06 16:00:53 +0530

    last three prime no in range

---
+++ Basic_Programs/src/com/Loop_NumberPrograms/LastThreePrimeNoInRange.java
@@ -0,0 +1,34 @@
+package com.Loop_NumberPrograms;
+
+import java.util.Scanner;
+
+public class LastThreePrimeNoInRange {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a start range: ");
+		int st = sc.nextInt();
+		System.out.println("Enter a end range: ");
+		int end = sc.nextInt();
+		int count = 0;
+		
+		for(int i=end; i>=st; i--) {
+			if(isPrime(i)) {
+				count++;
+				System.out.println(i);
+				if(count == 3) break;
+			}
+		}
+	}
+	
+	//prime no check
+	public static boolean isPrime(int n)
+	{
+		if(n <= 1) return false;
+		
+		for(int i=2; i<=n/2; i++) {
+			if(n%i == 0)
+				return false;
+		}
+		return true;
+	}
+}
