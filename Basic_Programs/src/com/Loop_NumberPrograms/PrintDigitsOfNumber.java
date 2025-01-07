c5d6ca926fdae22ba33fe1bddfd9797726bf63a7
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-07 15:12:02 +0530

    digits of a number

---
+++ Basic_Programs/src/com/Loop_NumberPrograms/PrintDigitsOfNumber.java
@@ -0,0 +1,19 @@
+package com.Loop_NumberPrograms;
+
+import java.util.Scanner;
+
+public class PrintDigitsOfNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int num = sc.nextInt();
+		
+		System.out.println("Digits of the given number"+num+" are: ");
+		while(num > 0) {
+			System.out.println(num%10+" ");
+			num /= 10;
+		}
+	}
+
+}
