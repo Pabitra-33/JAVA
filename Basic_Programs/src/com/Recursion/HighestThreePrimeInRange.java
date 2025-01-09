156a6cc24d187d8a2a9ac3307932cdd21c2d2758
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-09 19:09:39 +0530

    Highest three prime numbers in a range using recursion

---
+++ Basic_Programs/src/com/Recursion/HighestThreePrimeInRange.java
@@ -0,0 +1,31 @@
+package com.Recursion;
+
+import java.util.Scanner;
+
+public class HighestThreePrimeInRange {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter starting range: ");
+		int st = sc.nextInt();
+		System.out.println("Enter ending range: ");
+		int end = sc.nextInt();
+		System.out.print("Highest Prime Number between "+st+" to "+end+" is: ");
+		range(st, end, 0);
+	}
+	
+	public static void range(int start, int end, int count) {
+		if(start > end) return;
+		if(isPrime(end, end/2)) {
+			count++;
+			System.out.print(end+" ");
+			if(count >= 3) return;
+		}
+		range(start, end-1, count);
+	}
+	
+	public static boolean isPrime(int n, int i) {
+		if(i == 1) return true;
+		if(n<=1 || n%i==0) return false;
+		return isPrime(n, i-1);
+	}
+}
