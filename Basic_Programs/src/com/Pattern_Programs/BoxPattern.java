1409d92e422f6665ab14d31e2c02be0da5b33891
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-12 00:01:53 +0530

    Simple Square Box Pattern

--- Basic_Programs/src/com/Pattern_Programs/BoxPattern.java
+++ Basic_Programs/src/com/Pattern_Programs/BoxPattern.java
@@ -6,10 +6,11 @@ public class BoxPattern {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		for(int i=1; i<= n; i++) {
-			for(int j=1; j<=n; j++) {
-				System.out.print("*");
+		System.out.println("Enter the size: ");
+		int size = sc.nextInt();
+		for(int i=1; i<=size; i++) {
+			for(int j=1; j<=size; j++) {
+				System.out.print(" * ");
 			}
 			System.out.println();
 		}
