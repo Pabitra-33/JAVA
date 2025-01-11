4f672c144fd7e32e2175947035f6cd8bbbf61d50
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-11 23:32:18 +0530

    Armstrong Number in a range using method recursion

---
+++ Basic_Programs/src/com/Recursion/ArmstrongNoInRangeRec.java
@@ -0,0 +1,30 @@
+package com.Recursion;
+
+import java.util.Scanner;
+
+public class ArmstrongNoInRangeRec {
+	public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int first = s.nextInt();
+        System.out.println("Enter last number: ");
+        int last = s.nextInt();
+        
+        for(int i=first; i<=last; i++)
+        {
+        	int count = count(i);
+            int sum = isArmstrong(i,count);
+            if(sum == i) System.out.println(i+" ");
+        }
+    }
+
+    public static int isArmstrong(int n, int count){
+        if(n==0) return 0;
+        return (int)Math.pow(n%10,count)+isArmstrong(n/10,count);
+    }
+
+    public static int count(int n){
+        if(n == 0) return 0;
+        return 1 + count(n/10);
+    }
+}
