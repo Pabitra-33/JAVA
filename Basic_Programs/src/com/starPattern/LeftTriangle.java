813269c565a30f0ea517c869251f2e07bcc94da6
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-12 08:53:36 +0530

    Left Angled Triangle Pattern

---
+++ Basic_Programs/src/com/starPattern/LeftTriangle.java
@@ -0,0 +1,27 @@
+package com.starPattern;
+
+import java.util.Scanner;
+
+public class LeftTriangle {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a size: ");
+		int size = sc.nextInt();
+		int spaces = size-1;
+		
+		for(int i=1; i<=size; i++) {
+			//for printing spaces
+			for(int j=1; j<=spaces; j++) {
+				System.out.print(" ");
+			}
+			//for printing stars
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();//adds a new line
+			spaces--;
+		}
+	}
+
+}
