08e43e7e78ff16e523a9f50a0f09b8bd784e94cf
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-12 11:04:45 +0530

    Printing Automorphic Numbers in a given range using method recursion

---
+++ Basic_Programs/src/com/Recursion/AutomorphicNoInRangeRec.java
@@ -0,0 +1,33 @@
+package com.Recursion;
+
+import java.util.Scanner;
+
+public class AutomorphicNoInRangeRec {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first number: ");
+		int first = sc.nextInt();
+		System.out.println("Enter last number: ");
+		int last = sc.nextInt();
+		for(int i=first; i<= last; i++) {
+			if(isAutomorphic(i)) {
+				System.out.println(i+" ");
+			}
+		}
+	}
+	
+	public static boolean isAutomorphic(int num)
+	{
+		int sqr = num*num;
+		boolean flag = true;
+		while(num > 0) {
+			if(num%10 != sqr%10) 
+			{
+				return false;
+			}
+			sqr /= 10;
+			return true;
+		}
+		return isAutomorphic(num/10);
+	}
+}
