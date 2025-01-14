7b73efc30bb2fa4a7a895fcf1fc6fc37702e7ca9
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-14 23:08:56 +0530

    Printing Downward Star Pattern Program

---
+++ Basic_Programs/src/com/starPattern/DownwardPattern.java
@@ -0,0 +1,22 @@
+package com.starPattern;
+
+import java.util.Scanner;
+
+public class DownwardPattern {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter no of lines: ");
+		int line = scn.nextInt();
+		
+		//for iterating till no of lines/rows
+		for(int i=1; i<=line; i++) {
+			//for iterating no of columns
+			for(int j=i; j<=line; j++) {
+				System.out.print("* ");
+			}
+			//adding new line
+			System.out.println();
+		}
+	}
+}
