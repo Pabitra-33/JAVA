70960ff25e2cf8e0bd7fa71f6771ac99d8973bc2
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-14 23:15:16 +0530

    Printing Anti_Diagonal Star Pattern

---
+++ Basic_Programs/src/com/starPattern/Anti_DiagonalPattern.java
@@ -0,0 +1,27 @@
+package com.starPattern;
+
+import java.util.Scanner;
+
+public class Anti_DiagonalPattern {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the no of lines: ");
+		int lines = s.nextInt();
+		
+		//for no of rows/lines
+		for(int i=1; i<=lines; i++) {
+			//for no of columns
+			for(int j=1; j<=lines; j++) {
+				if(i==j || i+j==lines+1) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
