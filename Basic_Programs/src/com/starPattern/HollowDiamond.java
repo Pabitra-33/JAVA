79b8321f42eda72c87f7d25dcf9ecea78d7fd3c0
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-15 09:14:51 +0530

    Printing Hollow Diamond Star Pattern

---
+++ Basic_Programs/src/com/starPattern/HollowDiamond.java
@@ -0,0 +1,45 @@
+package com.starPattern;
+
+import java.util.Scanner;
+
+public class HollowDiamond {
+	public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter a line: ");
+        int lines = scn.nextInt();
+
+        int spaces = lines/2;
+        int stars = 1;
+
+        for(int i=1; i<= lines; i++)//for no of lines
+        {
+            for(int j=1; j<=spaces; j++)//for spaces
+            {
+                System.out.print(" ");
+            }
+            for(int j=1; j<= stars; j++)//for stars
+            {
+                if(j==1 || j==stars)
+                {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+                
+            }
+            System.out.println();
+            if(i<=lines/2)
+            {
+                spaces--;
+                stars+=2;
+            }
+            else{
+                spaces++;
+                stars-=2;
+            }
+        }
+    }
+}
