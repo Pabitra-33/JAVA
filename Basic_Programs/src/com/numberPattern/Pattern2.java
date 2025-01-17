380d5be0e1ff58e1c05b5dc7f083cb655f602f8b
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-17 17:28:02 +0530

    Second Number Pattern Program

--- Basic_Programs/src/com/numberPattern/Pattern2.java
+++ Basic_Programs/src/com/numberPattern/Pattern2.java
@@ -1,28 +1,31 @@
 package com.numberPattern;
 
+import java.util.Scanner;
+
 public class Pattern2 {
 	public static void printPattern(int n)
     {
-        int i, j;
         // outer loop to handle number of rows
-        for (i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             // inner loop to print space
-            for (j = 1; j <= n - i; j++) {
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
             // inner loop to print star
-            for (j = 1; j <= i; j++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(i + " ");
             }
             // print new line for each row
             System.out.println();
         }
     }
-
-    // Driver Function
-    public static void main(String args[])
+	
+	//main method
+	public static void main(String args[])
     {
-        int n = 6;
+		System.out.println("Enter a number: ");
+        int n = new Scanner(System.in).nextInt();
+        //calling the printPattern function to print the pattern.
         printPattern(n);
     }
 }
