47b4344e7df3147b166868a5638d6bf26e082d66
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-17 17:42:59 +0530

    Number pattern program prints the numbers in line by line by increasing one.

--- Basic_Programs/src/com/numberPattern/Pattern3.java
+++ Basic_Programs/src/com/numberPattern/Pattern3.java
@@ -1,13 +1,20 @@
 package com.numberPattern;
+import java.util.Scanner;
 
 public class Pattern3 {
 	public static void main(String[] args) {
-        int i, j, k = 1;
-        for (i = 1; i <= 5; i++) {
-            for (j = 1; j<=i + 1; j++) {
+        System.out.println("Enter a number: ");
+        int n = new Scanner(System.in).nextInt();
+        int k = 1;
+        
+        //outer loop 
+        for (int i = 1; i <= n; i++){  
+        	//inner loop
+        	for (int j = 1; j< i + 1; j++) {
+        		//prints the value of k  
                 System.out.print(k++ + " ");
-            }
-   
+            } 
+            //throws the cursor at the next line  
             System.out.println();
         }
     }
