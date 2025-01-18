b3800a58078514ebf0e1e42d41e67f18bc494a40
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-18 19:51:51 +0530

    Printing the numbers from 1 to given lines in alternate lines pattern

---
+++ Basic_Programs/src/com/numberPattern/Pattern4.java
@@ -0,0 +1,30 @@
+package com.numberPattern;
+
+import java.util.Scanner;
+
+public class Pattern4 {
+	public static void main(String[] args) {
+		int x=1, y=0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int n = sc.nextInt();
+		for(int i=1; i<=n; i++) {
+			//for odd lines
+			if(i%2==0) {
+				for(int j=1; j<=i; j++) {
+					System.out.print(x+" ");
+					x++;
+				}
+			}
+			else {
+				y = x+(i-1);
+				x = y+1;
+				for(int j=1; j<=i; j++) {
+					System.out.print(y+" ");
+					y--;
+				}
+			}
+			System.out.println();
+		}
+	}
+}
