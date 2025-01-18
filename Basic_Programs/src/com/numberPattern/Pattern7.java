f9236eca8147796f645462587809ce974a1abcbd
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-18 20:55:34 +0530

    Printing one and zero in this pattern in increasing order pattern

---
+++ Basic_Programs/src/com/numberPattern/Pattern7.java
@@ -0,0 +1,21 @@
+package com.numberPattern;
+import java.util.Scanner;
+
+public class Pattern7 {
+	public static void main(String[] args)   
+	{ 
+		System.out.println("Enter the no of rows: ");
+		int rows= new Scanner(System.in).nextInt();         
+		for (int i = 1; i <= rows; i++){  
+			for (int j = 1; j <= i; j++)  
+			{  
+				if(j%2 == 0){  
+					System.out.print(0);
+				}else  {  
+					System.out.print(1);
+				}  
+			}  
+	        System.out.println();  
+	    }  
+	}
+}
