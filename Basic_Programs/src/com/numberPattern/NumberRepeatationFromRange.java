c2fea88889b6e236a67d702ff17824e3ad8466ce
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-18 20:48:47 +0530

    Declarative from a given range to it's original one number pattern

---
+++ Basic_Programs/src/com/numberPattern/NumberRepeatationFromRange.java
@@ -0,0 +1,18 @@
+package com.numberPattern;
+import java.util.Scanner; 
+
+public class NumberRepeatationFromRange {
+	public static void main(String[] args)   
+	{    
+		Scanner sc = new Scanner(System.in);  
+		System.out.print("Enter the number of rows you want to print: ");  
+		int n = sc.nextInt();  
+		for (int i = 1; i <= n; i++)   
+		{   
+			for (int j = i; j >= 1; j--)  {  
+				System.out.print(j+" ");  
+			}  
+			System.out.println();  
+		}           
+	}
+}
