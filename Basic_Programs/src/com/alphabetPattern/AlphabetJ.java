3d9534681e402258413b74f444311abd36e39731
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-21 09:30:01 +0530

    Printed capital alphabet J pattern

---
+++ Basic_Programs/src/com/alphabetPattern/AlphabetJ.java
@@ -0,0 +1,19 @@
+package com.alphabetPattern;
+
+public class AlphabetJ {
+	public static void main(String[] args) {
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=5; j++)
+			{
+				if(i==1 || (i==5 & j<=3) || j==3) {
+					System.out.print(" *");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			//adds a new line
+			System.out.println();
+		}
+	}
+}
