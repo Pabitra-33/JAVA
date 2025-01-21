f10f762e7c1c44d1f1b4d972ba2c47e5d37c7715
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-21 09:41:58 +0530

    Printed capital alphabet K pattern

---
+++ Basic_Programs/src/com/alphabetPattern/AlphbetK.java
@@ -0,0 +1,19 @@
+package com.alphabetPattern;
+
+public class AlphbetK {
+	public static void main(String[] args) {
+		for(int i=1; i<=7; i++) {
+			for(int j=1; j<=4; j++)
+			{
+				if(j==1|| i+j==5 ||i-j==3) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			//adds a new line
+			System.out.println();
+		}
+	}
+}
