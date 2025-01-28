b8956a87c4c4bfa38469aa933dcd8345ea2f3e43
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-28 08:10:57 +0530

    Created a mobile class

---
+++ OOPS_Concept/src/com/ClassObject/Mobile.java
@@ -0,0 +1,19 @@
+package com.ClassObject;
+
+public class Mobile {
+	String name;
+	String color;
+	int ram;
+	int rom;
+	double price;
+	
+	//calling method
+	public void calling() {
+		System.out.println("Calling...");
+	}
+	
+	//texting method
+	public void texting() {
+		System.out.println("Texting...");
+	}
+}
