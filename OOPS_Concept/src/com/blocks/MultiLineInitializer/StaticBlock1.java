a9f3ddb8a0373742ec0b002226578a4d757f6ada
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-29 09:51:23 +0530

    Static blocks are executed before main method at the time of class loading

--- OOPS_Concept/src/com/blocks/MultiLineInitializer/StaticBlock1.java
+++ OOPS_Concept/src/com/blocks/MultiLineInitializer/StaticBlock1.java
@@ -2,9 +2,10 @@ package com.blocks.MultiLineInitializer;
 
 public class StaticBlock1 {
 	
-	//static blocks are always executed before main method
+	//static blocks are always executed before main method.
+	//first static block
 	static {
-		System.out.println("Static block executed");
+		System.out.println("1st Static block executed");
 	}
 	
 	//main method
@@ -12,5 +13,14 @@ public class StaticBlock1 {
 		System.out.println("Main Starts");
 		System.out.println("Main Ends");
 	}
-
+	
+	//second static block
+	static {
+		System.out.println("2nd Static block executed");
+	}
+	
+	//third static block
+	static {
+		System.out.println("3rd Static block executed");
+	}
 }
