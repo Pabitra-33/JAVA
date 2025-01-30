e8fefe6596787eb69cc6650220c43ecbb5103253
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-30 17:47:28 +0530

    Calling the default constructor each time by creating instance of the class by new keyword

--- OOPS_Concept/src/com/Constructor/A.java
+++ OOPS_Concept/src/com/Constructor/A.java
@@ -1,5 +1,16 @@
 package com.Constructor;
 
-public class A {
-
+public class A 
+{
+	//constructor created
+	public A() {
+		System.out.println("A-Class Constructor");
+	}
+	
+	public static void main(String[] args) {
+		A a1 = new A();//first instance
+		A a2 = new A();//second instance
+		A a3 = new A();//third instance
+		//when created a instance, internally constructor we are calling
+	}
 }
