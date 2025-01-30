c98828892d4e0889c389108ec43f27c317910c0b
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-30 17:40:17 +0530

    Created two static and three non-static block initializer and see their execution process.

---
+++ OOPS_Concept/src/com/blocks/MultiLineInitializer/StaticAndNonStaticTest.java
@@ -0,0 +1,34 @@
+package com.blocks.MultiLineInitializer;
+
+public class StaticAndNonStaticTest {
+	//static block-1
+		static {
+			System.out.println("Static block-1");
+			StaticAndNonStaticTest sn = new StaticAndNonStaticTest();
+		}
+		
+		//non-static block-1
+		{
+			System.out.println("Non-static block-1");
+		}
+		
+		//main method
+		public static void main(String[] args) {
+			System.out.println("Main Starts");
+			{
+				StaticAndNonStaticTest sn = new StaticAndNonStaticTest();
+			}
+			System.out.println("Main Ends");
+		}
+		
+		//non-static block-2
+		{
+			System.out.println("Non-Static block-2");
+		}
+		
+		//static block-2
+		static {
+			StaticAndNonStaticTest sn = new StaticAndNonStaticTest();
+			System.out.println("Static block-2");
+		}
+}
