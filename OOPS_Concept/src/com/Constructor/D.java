48f9d318a0e0ecc661290a2bca40581a0b9c924c
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-01 22:44:47 +0530

    implementing other class reference inside the constructor to see the execution difference

--- OOPS_Concept/src/com/Constructor/D.java
+++ OOPS_Concept/src/com/Constructor/D.java
@@ -1,19 +1,22 @@
 package com.Constructor;
 
 public class D {
-
+	//default D constructor
 	public D() {
 		System.out.println("Default D()-Constructor");
 	}
 
+	//parameterized D constructor
 	public D(int i) {
 		System.out.println("D(int i)-Constructor");
 	}
 
+	//parameterized D constructor with D class reference
 	public D(D d) {
 		System.out.println("D(D d)-Constructor");
 	}
 
+	//parameterized D constructor with A class reference
 	public D(A a) {
 		System.out.println("D(A a)-Constructor");
 	}
