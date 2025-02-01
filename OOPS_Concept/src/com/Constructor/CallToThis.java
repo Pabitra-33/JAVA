9138f3040489ecc1b4f0cae251c68d391fb9c9ee
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-01 20:28:59 +0530

    Using this() we can call same class another constructor

--- OOPS_Concept/src/com/Constructor/CallToThis.java
+++ OOPS_Concept/src/com/Constructor/CallToThis.java
@@ -1,5 +1,18 @@
 package com.Constructor;
 
-public class CallToThis {
+class Test
+{
+	Test(){
+		this(10);//parameterized same class constructor calling statement
+		System.out.println("Default Constructor");
+	}
+	Test(int i){
+		System.out.println("Parameterized Constructor");
+	}
+}
 
+public class CallToThis {
+	public static void main(String[] args) {
+		Test t = new Test();
+	}
 }
