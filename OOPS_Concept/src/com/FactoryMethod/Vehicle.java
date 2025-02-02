4b93521e829252e0bdf9e11b6be3ecdb8bf57850
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-03 01:01:30 +0530

    initial commit

---
+++ OOPS_Concept/src/com/FactoryMethod/Vehicle.java
@@ -0,0 +1,13 @@
+package com.FactoryMethod;
+
+public class Vehicle {
+	String name;
+	int vNo;
+	
+	//constructor
+	public Vehicle(String name, int vNo) {
+		this.name = name;
+		this.vNo = vNo;
+	}
+	
+}
