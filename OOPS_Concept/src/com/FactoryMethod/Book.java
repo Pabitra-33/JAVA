ab4b69030bb7e10689c5be1ff6b3b8463a39f02a
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-03 01:24:32 +0530

    initial commit

---
+++ OOPS_Concept/src/com/FactoryMethod/Book.java
@@ -0,0 +1,16 @@
+package com.FactoryMethod;
+
+public class Book {
+	String name;
+	String author;
+	double price;
+	
+	//argumented constructor
+	public Book(String name, String author, double price) {
+		this.name = name;
+		this.author = author;
+		this.price = price;
+	}
+	
+	//factory
+}
