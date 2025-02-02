c1aa3c707adb29aa1d76b3b6cfac327e3d38a1a6
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-02 19:59:57 +0530

    implemented copy constructor which will copy the first constructor value and prints the same

--- OOPS_Concept/src/com/copyConstructor/DemoCopy.java
+++ OOPS_Concept/src/com/copyConstructor/DemoCopy.java
@@ -15,4 +15,12 @@ public class DemoCopy {
 		this.j = d.j;
 	}
 	
+	//main method
+	public static void main(String[] args) {
+		DemoCopy d1 = new DemoCopy(100,200);
+		DemoCopy d2 = new DemoCopy(d1);//copy constructor
+		
+		System.out.println(d1.i+" , "+d1.j);
+		System.out.println(d2.i+" , "+d2.j);
+	}
 }
