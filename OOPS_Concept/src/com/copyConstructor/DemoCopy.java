1391895e2ac2a64bb82298bc0cd8be8d41c75f3d
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-02 19:56:15 +0530

    Demo constructor

--- OOPS_Concept/src/com/copyConstructor/DemoCopy.java
+++ OOPS_Concept/src/com/copyConstructor/DemoCopy.java
@@ -1,5 +1,18 @@
 package com.copyConstructor;
 
-public class DemoCopy {
 
+public class DemoCopy {
+	int i;
+	int j;
+	
+	public DemoCopy(int i, int j) {
+		this.i = i;
+		this.j = j;
+	}
+
+	public DemoCopy(DemoCopy d) {
+		this.i = d.i;
+		this.j = d.j;
+	}
+	
 }
