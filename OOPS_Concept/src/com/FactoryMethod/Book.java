707369e35196144add1f443f47cf92c98301a359
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-03 01:27:55 +0530

    added a factory method

--- OOPS_Concept/src/com/FactoryMethod/Book.java
+++ OOPS_Concept/src/com/FactoryMethod/Book.java
@@ -1,5 +1,7 @@
 package com.FactoryMethod;
 
+import java.util.Scanner;
+
 public class Book {
 	String name;
 	String author;
@@ -12,5 +14,18 @@ public class Book {
 		this.price = price;
 	}
 	
-	//factory
+	//factory method
+	public static Book createBook() {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter book name:");
+		String name = scn.next();
+		
+		System.out.println("Enter book author:");
+		String author = scn.next();
+		
+		System.out.println("Enter book price:");
+		double price = scn.nextDouble();
+		
+		return new Book(name, author, price);
+	}
 }
