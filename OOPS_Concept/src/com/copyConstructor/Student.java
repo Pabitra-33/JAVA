470e3d9620af5b59018c8e181b3e53f094f4e04e
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-02 20:01:10 +0530

    initial commit

---
+++ OOPS_Concept/src/com/copyConstructor/Student.java
@@ -0,0 +1,14 @@
+package com.copyConstructor;
+
+public class Student {
+	String name;
+	int age;
+	long mobile;
+	public Student(String name, int age, long mobile) {
+		this.name = name;
+		this.age = age;
+		this.mobile = mobile;
+	}
+	
+	
+}
