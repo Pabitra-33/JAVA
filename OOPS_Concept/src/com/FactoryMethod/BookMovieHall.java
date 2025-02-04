448f61d654c5153c97197ff404b633fb2fdcc2cc
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-04 19:00:18 +0530

    initial commit

---
+++ OOPS_Concept/src/com/FactoryMethod/BookMovieHall.java
@@ -0,0 +1,17 @@
+package com.FactoryMethod;
+
+//creating a SingleTon Class
+public class BookMovieHall {
+	private int seats = 50;
+	private static BookMovieHall bookmoviehall = null;
+	
+	//creating a private constructor
+	private BookMovieHall() {
+		
+	}
+	
+	//bookTicket
+	public void bookTicket(int n) {
+		
+	}
+}
