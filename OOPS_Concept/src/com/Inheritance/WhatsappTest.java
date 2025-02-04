291ff40d64027da898ef52d116b60a099a489e3d
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-05 01:04:22 +0530

    three classes performing multi-level inheritance

--- OOPS_Concept/src/com/Inheritance/WhatsappTest.java
+++ OOPS_Concept/src/com/Inheritance/WhatsappTest.java
@@ -1,5 +1,31 @@
 package com.Inheritance;
 
-public class WhatsappTest {
+//version-1
+class WhatsappVersion_1 {
+	//textMsg()
+	public void textMsg() {
+		System.out.println("Hello welcome to Coding world.. ");
+	}
+}
 
+//version-2
+class WhatsappVersion_2 extends WhatsappVersion_1 {
+	//audioMsg()
+	public void audioMsg() {
+		System.out.println("Hello you are in audio call... ");
+	}
+}
+
+//
+class WhatsappVersion_3 extends WhatsappVersion_2 {
+	//textMsg()
+	public void videoCall() {
+		System.out.println("You are in videocall....");
+	}
+}
+
+public class WhatsappTest {
+	public static void main(String[] args) {
+		
+	}
 }
