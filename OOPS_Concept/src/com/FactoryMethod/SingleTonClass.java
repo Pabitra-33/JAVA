232b966506c41485ca7678b1c862c797921827ef
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-04 18:50:40 +0530

    Created a Single ton class by making it's constructor private and using a factory method we create object and return it

--- OOPS_Concept/src/com/FactoryMethod/SingleTonClass.java
+++ OOPS_Concept/src/com/FactoryMethod/SingleTonClass.java
@@ -1,5 +1,27 @@
 package com.FactoryMethod;
 
+class A
+{
+	static A a = null;
+	int i = 60;
+	
+	//we can make a singleton class by making our constructor private
+	private A() {
+		
+	}
+	
+	//to create a object for private constructor we used factory method
+	public static A getInstance() {
+		//checking if the class has any object or not, if this is null then create one
+		if(a == null) {
+			a = new A();
+			return a;
+		}else {
+			return a;
+		}
+	}
+}
+
 public class SingleTonClass {
 	public static void main(String[] args) {
 		
