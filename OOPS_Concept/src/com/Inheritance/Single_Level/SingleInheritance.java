50597e3308f068d56ccb6bcb9b5d00bb91f519d5
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-05 09:57:30 +0530

    Performed single level inheritance in java

--- OOPS_Concept/src/com/Inheritance/Single_Level/SingleInheritance.java
+++ OOPS_Concept/src/com/Inheritance/Single_Level/SingleInheritance.java
@@ -1,6 +1,21 @@
 package com.Inheritance.Single_Level;
 
+class A {
+	int i = 20;
+}
+
+class B extends A{
+	int i = 30;
+	
+	public void display() {
+		System.out.println("Child class member i :"+i);
+		System.out.println("Parent Class member i:"+super.i);//referring parent class data member
+	}
+}
 
 public class SingleInheritance {
-
+	public static void main(String[] args) {
+		B b = new B();
+		b.display();
+	}
 }
