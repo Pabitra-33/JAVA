c00c7f57f3f0de92e64607b7688171fc6c2a2730
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-05 10:04:28 +0530

    New class name added to avoid naming collision inside the same package

--- OOPS_Concept/src/com/Inheritance/Single_Level/Test.java
+++ OOPS_Concept/src/com/Inheritance/Single_Level/Test.java
@@ -1,23 +1,23 @@
 package com.Inheritance.Single_Level;
 
 //created A class
-class A{
+class AA{
 	int i;
 }
 
 //created B class extending A class
-class B extends A{
+class BB extends A{
 	int j;
 }
 
 public class Test {
 	public static void main(String[] args) {
-		A a = new A();//parent class object created
+		AA a = new AA();//parent class object created
 		a.i = 10;
 		System.out.println(a.i);//10
 		//System.out.println(a.j);//CTE: we can't access child class specific properties using parent class object reference
 		
-		B b = new B();
+		BB b = new BB();
 		b.i = 30;
 		b.j = 60;
 		System.out.println(b.j);
