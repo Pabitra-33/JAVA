0c0b23868178568237bec4140fdab4d5258de5c2
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-06 19:28:36 +0530

    Created a Bicycle class having one subclass which refer to it's parent class peoperties

--- OOPS_Concept/src/com/Inheritance/Single_Level/Bicycle.java
+++ OOPS_Concept/src/com/Inheritance/Single_Level/Bicycle.java
@@ -1,5 +1,18 @@
 package com.Inheritance.Single_Level;
 
 public class Bicycle {
-
+	public int gear;
+	public int speed;
+	
+	//bicycle constructor
+	public Bicycle(int gear, int speed) {
+		this.gear = gear;
+		this.speed = speed;
+	}
+}
+class MountainBicycle extends Bicycle {
+	//MountainBicycle constructor
+	public MountainBicycle(int gear, int speed) {
+		super(gear, speed);
+	}
 }
