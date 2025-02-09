07a14a7c52d036b049f82aea2aadb21eed616e26
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-09 09:55:38 +0530

    combining with the try-with resource to implicitly close the scanner class reference connection

--- Basic_Programs/src/com/Loops/DoWhileLoop.java
+++ Basic_Programs/src/com/Loops/DoWhileLoop.java
@@ -6,19 +6,21 @@ public class DoWhileLoop {
 	
 	public static void main(String[] args) 
 	{
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a range: ");
-		int range = sc.nextInt();
-		
-		int i = 1;
-        int sum = 0;
-        
-        do {
-            sum += i;
-            i++;
-        } while (i <= range);
-        
-        System.out.println("Sum of numbers from 1 to "+range+" is: " + sum);
+		//surround with try-with resource
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter a range: ");
+			int range = sc.nextInt();
+			
+			int i = 1;
+			int sum = 0;
+			
+			do {
+			    sum += i;
+			    i++;
+			} while (i <= range);
+			
+			System.out.println("Sum of numbers from 1 to "+range+" is: " + sum);
+		}
 	}
 
 }
