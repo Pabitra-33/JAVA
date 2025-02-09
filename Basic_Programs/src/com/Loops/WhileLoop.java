7a010c24afcf8ace06b1c1771a6d806c3eaac6f9
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-09 10:56:24 +0530

    using the try-with resource for implicitly close the connection of a scanner reference

--- Basic_Programs/src/com/Loops/WhileLoop.java
+++ Basic_Programs/src/com/Loops/WhileLoop.java
@@ -6,14 +6,16 @@ public class WhileLoop {
 
 	public static void main(String[] args) {
 		//printing the numbers in a range given by user.
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a range: ");
-		int range = sc.nextInt();
-		int i = 1;
-		
-		while(i <= range) {
-			System.out.println(i+" ");
-			i++;
+		//used the try-with resource to implicitly close the connection
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter a range: ");
+			int range = sc.nextInt();
+			int i = 1;
+			
+			while(i <= range) {
+				System.out.println(i+" ");
+				i++;
+			}
 		}
 	}
 
