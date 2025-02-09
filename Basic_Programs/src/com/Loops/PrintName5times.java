ba91a0c02ec680b9dedddcbbb6a410ad18c7beac
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-09 10:12:12 +0530

    printing the name five times using loops and used the try-with resources to handle the close connection of the scanner reference

--- Basic_Programs/src/com/Loops/PrintName5times.java
+++ Basic_Programs/src/com/Loops/PrintName5times.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 public class PrintName5times {
 
 	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
-		String name = scn.nextLine();
-		
-		// for loop to print numbers from 1 to 5
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(name+" ");
-        }
-        System.out.println("Loop has ended.");
+		//used try-with resource for implicit close of connection
+		try (Scanner scn = new Scanner(System.in)) {
+			String name = scn.nextLine();
+			
+			// for loop to print name 5 times
+			for (int i = 1; i <= 5; i++) {
+			    System.out.println(name+" ");
+			}
+		}
+		System.out.println("Loop has ended.");
 	}
 }
