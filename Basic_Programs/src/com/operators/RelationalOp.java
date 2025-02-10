7c993ac8e269dcf2ee8923c95c68e3ab28a86201
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-10 08:01:48 +0530

    performing relational operations on the operands which returns the boolean result

--- Basic_Programs/src/com/operators/RelationalOp.java
+++ Basic_Programs/src/com/operators/RelationalOp.java
@@ -2,7 +2,9 @@ package com.operators;
 
 public class RelationalOp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		//these are some relational operators mostly used in program
 		System.out.println(10 < 20);//true
 		System.out.println(30 > 60);//false
 		System.out.println(40 >= 20);//true
