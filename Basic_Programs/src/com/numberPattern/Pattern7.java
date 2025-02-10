1b17183871a420f05c96c9894dd794c1f8f397da
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-10 07:50:12 +0530

    printing the continuous 1, 101 pattern sum and closing the scanner class reference connection

--- Basic_Programs/src/com/numberPattern/Pattern7.java
+++ Basic_Programs/src/com/numberPattern/Pattern7.java
@@ -5,10 +5,12 @@ public class Pattern7 {
 	public static void main(String[] args)   
 	{ 
 		System.out.println("Enter the no of rows: ");
-		int rows= new Scanner(System.in).nextInt();         
-		for (int i = 1; i <= rows; i++){  
-			for (int j = 1; j <= i; j++)  
-			{  
+		Scanner scanner = new Scanner(System.in);
+		int rows= scanner.nextInt();
+		//iterate a loop
+		for (int i = 1; i <= rows; i++)
+		{  
+			for (int j = 1; j <= i; j++){  
 				if(j%2 == 0){  
 					System.out.print(0);
 				}else  {  
@@ -16,6 +18,7 @@ public class Pattern7 {
 				}  
 			}  
 	        System.out.println();  
-	    }  
+	    }
+		scanner.close();//closing the scanner class reference connection
 	}
 }
