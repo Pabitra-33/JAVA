365a4cd89adf4d0f41714df89deb10b3b36bd665
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-11 08:06:07 +0530

    Created a method to print the highest Automorphic number in a given range using method recursion

--- Basic_Programs/src/com/Recursion/HighestAutomorphicRec.java
+++ Basic_Programs/src/com/Recursion/HighestAutomorphicRec.java
@@ -4,17 +4,19 @@ import java.util.Scanner;
 
 public class HighestAutomorphicRec {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter first number: ");
-		int start = sc.nextInt();
-		System.out.println("Enter last number: ");
-		int end = sc.nextInt();
-		
-		System.out.print("Highest Automorphic Number between "+start+" and "+end+" is: ");
-		for(int i=end; i>= start; i--) {
-			if(isAutomorphic(i)) {
-				System.out.println(i+" ");
-				break;
+		//surrounding with try-with resources
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter first number: ");
+			int start = sc.nextInt();
+			System.out.println("Enter last number: ");
+			int end = sc.nextInt();
+			
+			System.out.print("Highest Automorphic Number between "+start+" and "+end+" is: ");
+			for(int i=end; i>= start; i--) {
+				if(isAutomorphic(i)) {
+					System.out.println(i+" ");
+					break;
+				}
 			}
 		}
 	}
@@ -22,7 +24,6 @@ public class HighestAutomorphicRec {
 	public static boolean isAutomorphic(int num)
 	{
 		int sqr = num*num;
-		boolean flag = true;
 		while(num > 0) {
 			if(num%10 != sqr%10) 
 			{
