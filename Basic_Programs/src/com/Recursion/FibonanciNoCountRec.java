b482a38fccbaef065d4e8d59eb68aa0d22d5d9cc
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-11 07:27:35 +0530

    defined the fibonanciCountPrint method to print the fibonanci series or numbers to a certain count

--- Basic_Programs/src/com/Recursion/FibonanciNoCountRec.java
+++ Basic_Programs/src/com/Recursion/FibonanciNoCountRec.java
@@ -6,8 +6,10 @@ public class FibonanciNoCountRec {
 
 	public static void main(String[] args) {
 		System.out.println("Enter number of count:");
-		int n = new Scanner(System.in).nextInt();
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
 		fibonanciCountPrint(0,1,1,n);
+		input.close();//closing the connection
 	}
 	static void fibonanciCountPrint(int a, int b, int count, int n) {
 		if(count > n) return;
