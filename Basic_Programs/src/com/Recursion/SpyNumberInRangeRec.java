d8fff2f5574f3c7617b097eab954f0249482b76a
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-13 16:28:04 +0530

    printing the spy numbers in a given range using method recursion

--- Basic_Programs/src/com/Recursion/SpyNumberInRangeRec.java
+++ Basic_Programs/src/com/Recursion/SpyNumberInRangeRec.java
@@ -3,24 +3,23 @@ package com.Recursion;
 import java.util.Scanner;
 
 public class SpyNumberInRangeRec {
-
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter first range: ");
-		int first = sc.nextInt();
-		System.out.println("Enter last range: ");
-		int end = sc.nextInt();
-		
-		System.out.println("Spy numbers in between "+first+" and "+end+" are:");
-		for(int i=first; i<= end; i++) {
-			if(isSpy(i,0,1)) {
-				System.out.println(i+" ");
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter first range: ");
+			int first = sc.nextInt();
+			System.out.println("Enter last range: ");
+			int end = sc.nextInt();
+			
+			System.out.println("Spy numbers in between "+first+" and "+end+" are:");
+			for(int i = first; i<= end; i++) {
+				if(isSpy(i,0,1)) {
+					System.out.println(i+" ");
+				}
 			}
 		}
 	}
-	
-	public static boolean isSpy(int n, int sum, int prod)
-	{
+	//spy check method
+	public static boolean isSpy(int n, int sum, int prod){
 		if(n==0) {
 			if(sum == prod) return true;
 			else return false;
