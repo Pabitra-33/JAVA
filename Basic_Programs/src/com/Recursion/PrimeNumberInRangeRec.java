1f6f154d23744e992de9b475b669707dc9959cc5
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-13 08:54:47 +0530

    formatted the comments and closed the reference connection of scanner class

--- Basic_Programs/src/com/Recursion/PrimeNumberInRangeRec.java
+++ Basic_Programs/src/com/Recursion/PrimeNumberInRangeRec.java
@@ -11,14 +11,17 @@ public class PrimeNumberInRangeRec {
 		int end = sc.nextInt();
 		System.out.print("Prime Numbers between "+st+" and "+end+" are: ");
 		range(st, end);
+		//close the scanner reference connection
+		sc.close();
 	}
+	//range method
 	public static void range(int start, int end) {
 		if(start > end) return;
 		if(isPrime(start, start/2))
 			System.out.print(start+" ");
 		range(start+1, end);
 	}
-	
+	//prime check method
 	public static boolean isPrime(int n, int i) {
 		if(i==1) return true;
 		if(n<=1 || n%i == 0) return false;
