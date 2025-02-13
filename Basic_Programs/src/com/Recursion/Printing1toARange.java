1de5e8f559ed0ac9afe774a0ced8ab306456caab
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-13 16:20:01 +0530

    printing numbers from 1 to a given range using method recursion and used try-with resource for implicit close of reference connection

--- Basic_Programs/src/com/Recursion/Printing1toARange.java
+++ Basic_Programs/src/com/Recursion/Printing1toARange.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
 public class Printing1toARange {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a range: ");
-		int range = sc.nextInt();
-		
-		display(1, range);
+		//surrounding with try-with resource for implicit close of reference
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter a range: ");
+			int range = sc.nextInt();
+			//calling display method
+			display(1, range);
+		}
 	}
 	
 	static void display(int i, int range) {
-		if(i>range) 
-			return;
+		//base condition
+		if(i > range) return;
 		System.out.print(i+" ");
-		display(i+1, range);
+		display(i+1, range);//calling recursively
 	}
 
 }
