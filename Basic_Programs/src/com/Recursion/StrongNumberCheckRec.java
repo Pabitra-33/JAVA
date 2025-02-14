57e75bedb5ddb9cd1f5df78132100134e250e259
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-14 08:18:25 +0530

    Strong number using method recursion

--- Basic_Programs/src/com/Recursion/StrongNumberCheckRec.java
+++ Basic_Programs/src/com/Recursion/StrongNumberCheckRec.java
@@ -5,19 +5,20 @@ import java.util.Scanner;
 public class StrongNumberCheckRec {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a number: ");
-		int num = sc.nextInt();
-		
-		int sum = strong(num);
-		if(sum == num) {
-			System.out.println(num+" is a Strong Number");
-		}
-		else {
-			System.out.println(num+" is not a Strong Number");
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter a number: ");
+			int num = sc.nextInt();
+			//store and check with the original
+			int sum = strong(num);
+			if(sum == num) {
+				System.out.println(num+" is a Strong Number");
+			}
+			else {
+				System.out.println(num+" is not a Strong Number");
+			}
 		}
 	}
-
+	//calculate strong no
 	public static int strong(int num) {
 		if(num == 0) return 0;
 		return fact(num%10) + strong(num/10);
