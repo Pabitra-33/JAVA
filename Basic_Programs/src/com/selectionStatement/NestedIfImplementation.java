2311a4721e672b6394c99674103ad7b9267ae4e3
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-14 08:24:54 +0530

    added try-with resource and implemented nested if statements

--- Basic_Programs/src/com/selectionStatement/NestedIfImplementation.java
+++ Basic_Programs/src/com/selectionStatement/NestedIfImplementation.java
@@ -5,17 +5,18 @@ import java.util.Scanner;
 public class NestedIfImplementation {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter your age: ");
-		int age=sc.nextInt();
-		System.out.println("Enter your age: ");
-		int weight=sc.nextInt();
-		
-		//applying condition on age and weight by using nested-if 
-		if(age>=18){    
-			if(weight>50){  
-				System.out.println("You are eligible to donate blood");  
+		//try-with resources
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter your age: ");
+			int age=sc.nextInt();
+			System.out.println("Enter your age: ");
+			int weight=sc.nextInt();
+			
+			//applying condition on age and weight by using nested-if 
+			if(age>=18){    
+				if(weight>50){  
+					System.out.println("You are eligible to donate blood");  
+				}
 			}
 		}  
 	}
