9f38478a51f3b101b98645c278f39657c2d710de
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-14 08:20:59 +0530

    implementing the conditional statement if else

--- Basic_Programs/src/com/selectionStatement/If_ElseImplementation.java
+++ Basic_Programs/src/com/selectionStatement/If_ElseImplementation.java
@@ -7,16 +7,18 @@ import java.util.Scanner;
 public class If_ElseImplementation {
 
 	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
-		System.out.print("Enter a number: ");
-		int n = scn.nextInt();
-		
-		if(n % 2 == 0)
-		{
-			System.out.println(n+" is an Even Number");
-		}
-		else {
-			System.out.println(n+" is an Odd Number");
+		//try-with resource
+		try (Scanner scn = new Scanner(System.in)) {
+			System.out.print("Enter a number: ");
+			int n = scn.nextInt();
+			
+			if(n % 2 == 0)
+			{
+				System.out.println(n+" is an Even Number");
+			}
+			else {
+				System.out.println(n+" is an Odd Number");
+			}
 		}
 	}
 }
