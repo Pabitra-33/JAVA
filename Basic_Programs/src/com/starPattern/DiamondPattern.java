8eeabce02c4c8b94797f73f795f6d67803da4386
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-15 07:25:14 +0530

    printing the diamond pattern

--- Basic_Programs/src/com/starPattern/DiamondPattern.java
+++ Basic_Programs/src/com/starPattern/DiamondPattern.java
@@ -5,34 +5,35 @@ import java.util.Scanner;
 public class DiamondPattern {
 
 	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
+		//try-with resources
+		try (Scanner scn = new Scanner(System.in)) {
+			System.out.println("Enter no of line: ");
+			int lines = scn.nextInt();
 
-        System.out.println("Enter no of line: ");
-        int lines = scn.nextInt();
+			int spaces = lines/2;
+			int stars = 1;
 
-        int spaces = lines/2;
-        int stars = 1;
-
-        for(int i=1; i<= lines; i++)//for no of lines
-        {
-            for(int j=1; j<=spaces; j++)//for spaces
-            {
-                System.out.print(" ");
-            }
-            for(int j=1; j<= stars; j++)//for stars
-            {
-                System.out.print("*");
-            }
-            System.out.println();
-            if(i<=lines/2)
-            {
-                spaces--;
-                stars+=2;
-            }
-            else{
-                spaces++;
-                stars-=2;
-            }
-        }
+			for(int i=1; i<= lines; i++)//for no of lines
+			{
+			    for(int j=1; j<=spaces; j++)//for spaces
+			    {
+			        System.out.print(" ");
+			    }
+			    for(int j=1; j<= stars; j++)//for stars
+			    {
+			        System.out.print("*");
+			    }
+			    System.out.println();
+			    if(i<=lines/2)
+			    {
+			        spaces--;
+			        stars+=2;
+			    }
+			    else{
+			        spaces++;
+			        stars-=2;
+			    }
+			}
+		}
 	}
 }
