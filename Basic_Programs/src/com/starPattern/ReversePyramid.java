9d2abf92573400236d89d09829e6f9d5f4a7373d
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-15 09:01:27 +0530

    printing reverse pyramid using for loop in java

--- Basic_Programs/src/com/starPattern/ReversePyramid.java
+++ Basic_Programs/src/com/starPattern/ReversePyramid.java
@@ -4,22 +4,24 @@ import java.util.Scanner;
 
 public class ReversePyramid {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter no of lines: ");
-		int lines = sc.nextInt();
-		
-		//for the no of lines/rows
-		for(int i=0; i<=lines-1; i++) {
-			//printing space
-			for(int j=0; j<=i; j++) {
-				System.out.print(" ");
+		//try-with resources
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter no of lines: ");
+			int lines = sc.nextInt();//user input
+			
+			//for the no of lines/rows
+			for(int i=0; i<=lines-1; i++) {
+				//printing space
+				for(int j=0; j<=i; j++) {
+					System.out.print(" ");
+				}
+				//printing stars
+				for(int j=0; j<=lines-1-i; j++) {
+					System.out.print("* ");
+				}
+				//add a new line for print
+				System.out.println();
 			}
-			//printing stars
-			for(int j=0; j<=lines-1-i; j++) {
-				System.out.print("* ");
-			}
-			//add a new line
-			System.out.println();
 		}
 	}
 
