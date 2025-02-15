54c6514099520511ff3aad2fe1c234b7f7497da2
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-15 08:55:44 +0530

    printing left triangle star pattern in java

--- Basic_Programs/src/com/starPattern/LeftTriangle.java
+++ Basic_Programs/src/com/starPattern/LeftTriangle.java
@@ -3,24 +3,25 @@ package com.starPattern;
 import java.util.Scanner;
 
 public class LeftTriangle {
-
+	//main method to start execution of the program
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a size: ");
-		int size = sc.nextInt();
-		int spaces = size-1;
-		
-		for(int i=1; i<=size; i++) {
-			//for printing spaces
-			for(int j=1; j<=spaces; j++) {
-				System.out.print(" ");
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter a size: ");
+			int size = sc.nextInt();
+			int spaces = size-1;//set spaces
+			
+			for(int i=1; i<=size; i++) {
+				//for printing spaces
+				for(int j=1; j<=spaces; j++) {
+					System.out.print(" ");
+				}
+				//for printing stars
+				for(int j=1; j<=i; j++) {
+					System.out.print("*");
+				}
+				System.out.println();//adds a new line
+				spaces--;
 			}
-			//for printing stars
-			for(int j=1; j<=i; j++) {
-				System.out.print("*");
-			}
-			System.out.println();//adds a new line
-			spaces--;
 		}
 	}
 
