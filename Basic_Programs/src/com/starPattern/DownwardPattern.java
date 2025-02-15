958a659c6780e3b4b8796b832438ed6a331244c7
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-15 08:45:35 +0530

    printing the downward pyramid star pattern

--- Basic_Programs/src/com/starPattern/DownwardPattern.java
+++ Basic_Programs/src/com/starPattern/DownwardPattern.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
 public class DownwardPattern {
 
 	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
-		System.out.println("Enter no of lines: ");
-		int line = scn.nextInt();//no of line input
-		
-		//for iterating till no of lines/rows
-		for(int i=1; i<=line; i++) {
-			//for iterating no of columns
-			for(int j=i; j<=line; j++) {
-				System.out.print("* ");
+		//surrounding with try-with resources
+		try (Scanner scn = new Scanner(System.in)) {
+			System.out.println("Enter no of lines: ");
+			int line = scn.nextInt();//no of line input
+			
+			//for iterating till no of lines/rows
+			for(int i=1; i<=line; i++) {
+				//for iterating no of columns
+				for(int j=i; j<=line; j++) {
+					System.out.print("* ");
+				}
+				//adding new line for print
+				System.out.println();
 			}
-			//adding new line
-			System.out.println();
 		}
 	}
 }
