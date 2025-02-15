c38858e8cb25a9f90e0a5c10fffeda2c41978173
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-15 08:53:41 +0530

    java program to print hollow triangle star pattern

--- Basic_Programs/src/com/starPattern/HollowTriangle.java
+++ Basic_Programs/src/com/starPattern/HollowTriangle.java
@@ -5,28 +5,31 @@ import java.util.Scanner;
 public class HollowTriangle {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a number: ");
-		int n = sc.nextInt();
-		int spaces = n-1;
-		int stars = 1;
-				
-		for(int i=1; i<=n; i++) //for number of lines
-		{
-			for(int j=1; j<=spaces; j++) {
-				System.out.print(" ");
-			}
-			for(int j=1; j<=stars; j++) {
-				if(i==n || j==1 || j==stars) {
-					System.out.print("*");
-				}
-				else {
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter a number: ");
+			int n = sc.nextInt();
+			int spaces = n-1;
+			int stars = 1;
+					
+			for(int i=1; i<=n; i++) //for number of lines
+			{
+				//to print spaces
+				for(int j=1; j<=spaces; j++) {
 					System.out.print(" ");
 				}
+				//to print star
+				for(int j=1; j<=stars; j++) {
+					if(i==n || j==1 || j==stars) {
+						System.out.print("*");
+					}
+					else {
+						System.out.print(" ");
+					}
+				}
+				System.out.println();
+				spaces--;
+				stars +=2;
 			}
-			System.out.println();
-			spaces--;
-			stars +=2;
 		}
 	}
 
