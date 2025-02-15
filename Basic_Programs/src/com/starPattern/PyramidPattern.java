795949cd21b9afbb834792ca548b06f369b8f072
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-15 08:59:07 +0530

    printing pyramid start pattern in java with the help of for loop

--- Basic_Programs/src/com/starPattern/PyramidPattern.java
+++ Basic_Programs/src/com/starPattern/PyramidPattern.java
@@ -3,26 +3,26 @@ package com.starPattern;
 import java.util.Scanner;
 
 public class PyramidPattern {
+	//main method
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the size: ");
-		int n = sc.nextInt();
-		
-		//i is for rows & j is for columns
-		for(int i=0; i<n; i++) {
+		//try-with resources
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter the size: ");
+			int n = sc.nextInt();
 			
-			//inner loop for printing space
-			for(int j=n-i; j>1; j--) {
-				System.out.print(" ");
+			//i is for rows & j is for columns
+			for(int i=0; i<n; i++) {
+				//inner loop for printing space
+				for(int j=n-i; j>1; j--) {
+					System.out.print(" ");
+				}
+				//inner loop for printing star
+				for(int j=0; j<=i; j++) {
+					System.out.print("* ");
+				}
+				//throws the cursor in a new line after printing each line  
+				System.out.println();
 			}
-			
-			//inner loop for printing star
-			for(int j=0; j<=i; j++) {
-				System.out.print("* ");
-			}
-			
-			//throws the cursor in a new line after printing each line  
-			System.out.println();
 		}
 	}
 }
