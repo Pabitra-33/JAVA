19e0df610874fa0faf4c5962e6cf0321f38f64e1
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-15 08:50:30 +0530

    written a program to print hollow square pattern

--- Basic_Programs/src/com/starPattern/HollowSquarePattern.java
+++ Basic_Programs/src/com/starPattern/HollowSquarePattern.java
@@ -3,27 +3,28 @@ package com.starPattern;
 import java.util.Scanner;
 
 public class HollowSquarePattern {
-
+	//main method
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the no of lines: ");
-		int lines = sc.nextInt();
-		
-		//for no of lines/rows
-		for(int i=1; i<=lines; i++) {
-			//for the columns
-			for(int j=1; j<=lines; j++) {
-				//conditions for where to print * and where to print ""
-				if(i==1 || j==1 || i==lines || j==lines) {
-					System.out.print("*");
+		try (Scanner sc = new Scanner(System.in)) {
+			//taking user input
+			System.out.println("Enter the no of lines: ");
+			int lines = sc.nextInt();
+			
+			//for no of lines/rows
+			for(int i=1; i<=lines; i++) {
+				//for the columns
+				for(int j=1; j<=lines; j++) {
+					//conditions for where to print * and where to print ""
+					if(i==1 || j==1 || i==lines || j==lines) {
+						System.out.print("*");
+					}
+					else {
+						System.out.print(" ");
+					}
 				}
-				else {
-					System.out.print(" ");
-				}
+				//adds a nextLine
+				System.out.println();
 			}
-			//adds a nextLine
-			System.out.println();
 		}
 	}
-
 }
