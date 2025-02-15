07b35332038a948d4870a579d0d794b60e00bd0d
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-15 07:24:32 +0530

    star pattern as a box printed

--- Basic_Programs/src/com/starPattern/BoxPattern.java
+++ Basic_Programs/src/com/starPattern/BoxPattern.java
@@ -4,14 +4,16 @@ import java.util.Scanner;
 
 public class BoxPattern {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the size: ");
-		int size = sc.nextInt();
-		for(int i=1; i<=size; i++) {
-			for(int j=1; j<=size; j++) {
-				System.out.print(" * ");
+		//try-with resources
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter the size: ");
+			int size = sc.nextInt();
+			for(int i=1; i<=size; i++) {
+				for(int j=1; j<=size; j++) {
+					System.out.print(" * ");//prints star
+				}
+				System.out.println();
 			}
-			System.out.println();
 		}
 	}
 }
