45eafd5e5c5f87b0c086ccb27a693e5edcd91e7d
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-16 12:26:27 +0530

    created right angled star pattern using loop and try-with resources to close the connection

--- Basic_Programs/src/com/starPattern/RightTraingle.java
+++ Basic_Programs/src/com/starPattern/RightTraingle.java
@@ -3,21 +3,21 @@ package com.starPattern;
 import java.util.Scanner;
 
 public class RightTraingle {
-
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a size: ");
-		int size = sc.nextInt();
-		
-		//outer loop to print the no of row data
-		for(int i=1; i<=size; i++)
-		{
-			//inner loop to print the no of column data
-			for(int j=1; j<=i; j++) {
-				System.out.print("*");
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter a size: ");
+			int size = sc.nextInt();//user input
+			
+			//outer loop to print the no of row data
+			for(int i=1; i<=size; i++)
+			{
+				//inner loop to print the no of column data
+				for(int j=1; j<=i; j++) {
+					System.out.print("*");
+				}
+				//adds a new line
+				System.out.println();
 			}
-			//adds a new line
-			System.out.println();
 		}
 	}
 }
