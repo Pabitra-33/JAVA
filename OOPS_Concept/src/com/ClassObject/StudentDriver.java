1fbf31265072296ff0b9a3354605b7e977adbd5a
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-17 07:18:39 +0530

    studentDriver class to execute the methods and properties of the student class

--- OOPS_Concept/src/com/ClassObject/StudentDriver.java
+++ OOPS_Concept/src/com/ClassObject/StudentDriver.java
@@ -4,14 +4,14 @@ public class StudentDriver {
 
 	public static void main(String[] args) {
 		Student s1 = new Student();
-		s1.insti = "JSP";
+		Student.insti = "JSP";//s1.insti = "JSP";
 		s1.name = "Ramana";
 		s1.age = 25;
 		s1.id = 1;
 		s1.mobile = 9564327691l;
 		
 		Student s2 = new Student();
-		s2.insti = "QSP";
+		Student.insti = "QSP";//s2.insti = "QSP";
 		s2.name = "Kavya";
 		s2.age = 23;
 		s2.id = 3;
