62d5f8f6095cdb0bd1b798a3ffc592f005349935
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-18 14:07:31 +0530

    multiple inheritance is not allowed in java using class for diamond problem

--- OOPS_Concept/src/com/Inheritance/Multiple/Main.java
+++ OOPS_Concept/src/com/Inheritance/Multiple/Main.java
@@ -1,28 +1,22 @@
 package com.Inheritance.Multiple;
 
-class A
-{
-    public void execute()
-    {
+class A{
+    public void execute(){
         System.out.println("Hi.. Executing From Class A");
     }
 }
-class B
-{
-    public void execute()
-    {
+class B{
+    public void execute(){
         System.out.println("Hi.. Executing From Class B");
     }
 }
 //class C extends A, B //Compile Time Error(multiple inheritance)
-class C extends A
-{
+class C extends A {
     
 }
 public class Main
 {
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
         C obj = new C(); // creating object of class C
         obj.execute(); // execute() method is present in both class A and B
 	}
