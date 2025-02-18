75375721466c5c7275285756006ffe2de25ba415
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-18 20:17:01 +0530

    performing multiple inheritance using interface as java allows multiple inheritance through interface

--- OOPS_Concept/src/com/Inheritance/Multiple/MultipleInheritanceUsingInterface.java
+++ OOPS_Concept/src/com/Inheritance/Multiple/MultipleInheritanceUsingInterface.java
@@ -1,5 +1,32 @@
 package com.Inheritance.Multiple;
 
-public class MultipleInheritanceUsingInterface {
+//interface character
+interface Character {  
+    void attack(); //attack method 
+}  
+//interface weapon
+interface Weapon {  
+    void use();//use method  
+}
 
+//performing multiple inheritance
+class Warrior implements Character, Weapon{
+	//overriding unimplemented class
+	@Override
+	public void use() {
+		System.out.println("Warrior uses a sword to attack");
+	}
+
+	@Override
+	public void attack() {
+		System.out.println("Warrior attacks with a sword");	
+	}
+}
+
+public class MultipleInheritanceUsingInterface{
+	public static void main(String[] args) {
+		Warrior warrior = new Warrior();
+		warrior.attack();//Warrior attacks with a sword
+		warrior.use();//Warrior uses a sword to attack
+	}
 }
