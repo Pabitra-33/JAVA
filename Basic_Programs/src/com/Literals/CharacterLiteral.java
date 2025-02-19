aca260e1a7ef5e1eb66b3c1aff94ea04cfbe875c
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-19 16:30:52 +0530

    implemented character literal use in our program and the valid operations which are allowed

---
+++ Basic_Programs/src/com/Literals/CharacterLiteral.java
@@ -0,0 +1,13 @@
+package com.Literals;
+
+public class CharacterLiteral {
+	public static void main(String[] args) {
+		char ch = 65;
+		System.out.println("Character value of given number is= "+ch);//A
+		
+		//char a= 65547; //Invalid CTE: incompatible types: possible lossy conversion from int to char.
+		
+		//char a= '65536';//CTE: invalid character constant
+		//System.out.println(a)
+	}
+}
