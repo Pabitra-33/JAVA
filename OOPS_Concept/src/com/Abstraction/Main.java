a31985846e6de5c389d8df22d47fddcaa8d327ea
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-01 22:17:19 +0530

    initial commit

---
+++ OOPS_Concept/src/com/Abstraction/Main.java
@@ -0,0 +1,30 @@
+package com.Abstraction;
+
+//Demonstrating Abstraction
+abstract class Light {
+	abstract void turnOn();
+    abstract void turnOff();
+}
+
+//Concrete class implementing the abstract methods
+class TVRemote extends Light {
+	@Override
+    void turnOn() {
+		System.out.println("TV is turned ON.");
+	}
+
+	@Override
+    void turnOff() {
+        System.out.println("TV is turned OFF.");
+    }
+}
+
+//Main class to demonstrate abstraction
+public class Main {
+	public static void main(String[] args) {
+     Light remote = new TVRemote();
+     remote.turnOn();   
+     remote.turnOff();  
+ }
+}
+
