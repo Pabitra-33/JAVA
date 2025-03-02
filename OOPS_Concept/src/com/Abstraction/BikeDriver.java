4d1b446c29fd269ac60abef6e116c3eba2e2ad05
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-02 20:15:30 +0530

    abstraction implementation in java using a  abstract class and overriding it's method

--- OOPS_Concept/src/com/Abstraction/BikeDriver.java
+++ OOPS_Concept/src/com/Abstraction/BikeDriver.java
@@ -1,17 +1,22 @@
 package com.Abstraction;
 
+//abstract class
 abstract class Bike {
 	abstract void run();
 }
+
+//subclass extending the abstract class
 class Honda extends Bike {
+	//providing implementation for the abstract class method by overriding it
 	void run() {
-		System.out.println("running safely");
+		System.out.println("Running Safely");
 	}
 }
-  
+ 
+//driver class to implementation class
 public class BikeDriver {
 	public static void main(String args[]){  
-		 Bike obj = new Honda();  
+		 Bike obj = new Honda();//upcasting  
 		 obj.run();  
 	}
 }  
