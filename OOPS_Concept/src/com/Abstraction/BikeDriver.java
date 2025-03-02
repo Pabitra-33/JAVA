391bd153560221a3c12cec212defbcd5cf09fd6f
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-02 10:44:42 +0530

    initial commit

---
+++ OOPS_Concept/src/com/Abstraction/BikeDriver.java
@@ -0,0 +1,16 @@
+package com.Abstraction;
+
+abstract class Bike {
+	abstract void run();
+}
+
+public class BikeDriver {
+
+} 
+class Honda4 extends Bike{  
+	void run(){System.out.println("running safely");}  
+	public static void main(String args[]){  
+	 Bike obj = new Honda4();  
+	 obj.run();  
+	}  
+	}  
