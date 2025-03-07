77ff4b6387181cbae88425506e2b8ec29dda57f5
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-07 22:03:05 +0530

    implementing the singleton class concept in our program to see and perform the single instance object creation

--- OOPS_Concept/src/com/FactoryMethod/singelTonClass/SingleTonClass.java
+++ OOPS_Concept/src/com/FactoryMethod/singelTonClass/SingleTonClass.java
@@ -1,18 +1,18 @@
-package com.FactoryMethod;
+package com.FactoryMethod.singelTonClass;
 
-class A
-{
+class A {
 	static A a = null;
 	int i = 60;
 	
-	//we can make a singleton class by making our constructor private
+	//we can make a singleton class by making 
+	//our constructor private so that not everyone can create object
 	private A() {
-		
 	}
 	
 	//to create a object for private constructor we used factory method
 	public static A getInstance() {
-		//checking if the class has any object or not, if this is null then create one
+		//checking if the class has any object or not,
+		//if this is null then create one
 		if(a == null) {
 			a = new A();
 			return a;
