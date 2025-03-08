bd494aa6feeedfaa956b50d7403e2ea8a0d57882
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-08 16:31:12 +0530

    performing the state of polymorphism where one object is capable of behaving multiple form

--- OOPS_Concept/src/com/Polymorphism/TestPolymorphism.java
+++ OOPS_Concept/src/com/Polymorphism/TestPolymorphism.java
@@ -7,13 +7,18 @@ class Person {
 	}
 }
 
-//child class
-class Father extends Person {
+//Derived class Father that overrides the role method.
+class Father extends Person 
+{
 	@Override
 	void role() {
 		System.out.println("I am a Father..!");
 	}
 }
-public class TestPolymorphism {
 
+public class TestPolymorphism {
+	public static void main(String[] args) {
+		Person p = new Father();
+		p.role();//it will call the overridden father method
+	}
 }
