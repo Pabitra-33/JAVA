7b7aaab43c166ea70a2851bc1c71f589fda7d1d9
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-12 20:32:15 +0530

    created a class to implement encapsulation in java

---
+++ OOPS_Concept/src/com/Encapsulation/EmployeeDriver.java
@@ -0,0 +1,25 @@
+package com.Encapsulation;
+
+class Employee {
+	private int empId;
+	private String eName;
+	
+	//Setter method
+	public void set_id(int empId) {
+		this.empId = empId;
+	}
+	public void set_name(String eName) {
+		this.eName = eName;
+	}
+	
+	//Getter method
+	public int get_Id() {
+		return empId;
+	}
+	public String get_Name() {
+		return eName;
+	}
+}
+public class EmployeeDriver {
+	
+}
