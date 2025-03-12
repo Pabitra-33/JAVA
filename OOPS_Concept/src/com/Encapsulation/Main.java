523cd74cdab4585e52a8b14e1781692be7b26828
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-12 23:36:26 +0530

    rectangle area finding using the getter and setter method in the encapsulation

--- OOPS_Concept/src/com/Encapsulation/Main.java
+++ OOPS_Concept/src/com/Encapsulation/Main.java
@@ -1,28 +1,25 @@
 package com.Encapsulation;
 
 class Area {
-    private int l; // this value stores length
-    private int b; // this value stores breadth
+    private int length; // this value stores length
+    private int breadth; // this value stores breadth
 
     // constructor to initialize values
-    Area(int l, int b)
-    {
-        this.l = l;
-        this.b = b;
+    Area(int l, int b) {
+        this.length = l;
+        this.breadth = b;
     }
 
     // method to calculate area
-    public void getArea()
-    {
-        int area = l * b;
+    public void getArea() {
+        int area = length * breadth;
         System.out.println("Area: " + area);
     }
 }
 
 public class Main {
-    public static void main(String[] args)
-    {
-        Area rect = new Area(2, 16);
-        rect.getArea();
+    public static void main(String[] args) {
+        Area rectangle = new Area(2, 16);
+        rectangle.getArea();
     }
 }
