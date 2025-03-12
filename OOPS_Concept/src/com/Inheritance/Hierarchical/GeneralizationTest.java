37658a6776cb44569d4eade7792c1bad51d067ab
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-12 12:39:20 +0530

    performing generalization in java

--- OOPS_Concept/src/com/Inheritance/Hierarchical/GeneralizationTest.java
+++ OOPS_Concept/src/com/Inheritance/Hierarchical/GeneralizationTest.java
@@ -2,37 +2,38 @@ package com.Inheritance.Hierarchical;
 
 //Generalized Superclass
 class Vehicle {
- int speed;
- String color;
- void start() {
-     System.out.println("Vehicle is starting");
- }
- void stop() {
-     System.out.println("Vehicle is stopping");
- }
+	int speed;
+	String color;
+	//start method
+	void start() {
+		System.out.println("Vehicle is starting");
+	}
+    void stop() {
+       System.out.println("Vehicle is stopping");
+    }
 }
 
-//Specific Subclasses
+//Subclasses
 class Car extends Vehicle {
- void openSunroof() {
-     System.out.println("Sunroof is open");
- }
+	void openSunroof() {
+		System.out.println("Sunroof is open");
+	}
 }
 
 class Bike extends Vehicle {
- void wheelie() {
-     System.out.println("Bike is doing a wheelie");
- }
+	void wheelie() {
+		System.out.println("Bike is doing a wheelie");
+	}
 }
 
 public class GeneralizationTest {
- public static void main(String[] args) {
-     Car myCar = new Car();
-     myCar.start(); // Output: Vehicle is starting
-     myCar.openSunroof();
+	public static void main(String[] args) {
+		Car myCar = new Car();
+        myCar.start(); // Output: Vehicle is starting
+        myCar.openSunroof();
 
-     Bike myBike = new Bike();
-     myBike.start(); // Output: Vehicle is starting
-     myBike.wheelie();
- }
+        Bike myBike = new Bike();
+        myBike.start(); // Output: Vehicle is starting
+        myBike.wheelie();
+    }
 }
