3113c95b5d1ed773334772362ac2d8ea0220ec71
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-14 01:26:01 +0530

    performing linear search operation in java to find a element in the array

--- src/com/Searching/LinearSearch.java
+++ src/com/Searching/LinearSearch.java
@@ -10,7 +10,7 @@ public class LinearSearch {
 	private static int search(int[] arr, int key) {
 		for(int i=0; i<arr.length; i++) {
 			if(arr[i] == key)
-				return i;//element found index
+				return i;//element found index position
 		}
 		return -1;//element not found
 	}
