d192a52ffeb064e0ae33c086b1a63ba0a4439e13
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-14 01:30:27 +0530

    linear searching

--- src/com/Searching/LinearSearch.java
+++ src/com/Searching/LinearSearch.java
@@ -3,6 +3,7 @@ package com.Searching;
 public class LinearSearch {
 	public static void main(String[] args){
         int[] arr = {1,3,5,2,4,6,7};
+        
         System.out.println(search(arr, 4));
         System.out.println(search(arr,9));
     }
