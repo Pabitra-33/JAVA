ca331debb0a44c855eec3a54776cc5b690bc9fa2
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-14 02:16:46 +0530

    variable basics

---
+++ Basics_Java/src/com/basics/Variables.java
@@ -0,0 +1,18 @@
+package com.basics;
+
+public class Variables {
+ 	static int j = 10;//static variable
+ 	String name = "Kailash";//non-static or instance variable
+ 	
+ 	public static void main(String[] args) {
+ 		int k = 5;//local variables
+ 		System.out.println(k);//5
+ 		//by using className we can refer the static variables in java
+ 		System.out.println(Variables.j);//10
+ 		
+ 		//to access non-static variables we need to create instance of the class.
+ 		//and by using that reference we can refer the instance variables of a class.
+ 		Variables v = new Variables();
+ 		System.out.println(v.name);//"Kailash"
+ 	}
+ }
