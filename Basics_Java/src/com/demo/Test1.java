ff0880af96e497cb02225ebd8145af802b6a1c45
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-14 02:12:41 +0530

    initial test

---
+++ Basics_Java/src/com/demo/Test1.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class Test1 {
+	public static void main(String[] args) {
+ 		System.out.println("main() of Test1 class");
+ 		Y.main();
+ 	}
+ }
+ class Y
+ {
+ 	public static void main() {
+ 		System.out.println("main() of Y class");
+ 	}
+}
