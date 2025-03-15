8505a26fe0eb3b96a88a30e258ef9966ccd8ba72
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-15 21:08:14 +0530

    operators in java

---
+++ Basics_Java/src/com/operators/LogicalOperator.java
@@ -0,0 +1,21 @@
+package com.operators;
+
+public class LogicalOperator {
+	public static void main(String[] args) {
+ 		//bitwise operator used to internally valid by converting their binary number.
+ 		int a = 5;
+ 		int b = 7;
+ 		
+ 		int res = a & b;//bitwise and
+ 		System.out.println(res);//5
+ 		
+ 		int res1 = a | b;//bitwise or
+ 		System.out.println(res1);//7
+ 		
+ 		int res2 = a ^ b;//bitwise x-or
+ 		System.out.println(res2);//2
+ 		
+ 		int res3 = ~ a;//bitwise not
+ 		System.out.println(res3);//-6
+ 	}
+}
