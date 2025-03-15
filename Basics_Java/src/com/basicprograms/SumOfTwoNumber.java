cf050165bdd75221fd63d26356d5ce9e19fe5eb8
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-15 22:39:54 +0530

    taking two user input calculate the sum and print it

---
+++ Basics_Java/src/com/basicprograms/SumOfTwoNumber.java
@@ -0,0 +1,19 @@
+package com.basicprograms;
+
+import java.util.Scanner;
+
+public class SumOfTwoNumber {
+	public static void main(String[] args) {
+ 		Scanner sc = new Scanner(System.in);
+ 		System.out.println("Enter first number: ");
+ 		int first = sc.nextInt();
+ 		System.out.println("Enter second number: ");
+ 		int second = sc.nextInt();
+ 		
+ 		int sum = 0;//to calculate sum of two no and store it in a variable.
+ 		sum = first + second;
+ 		System.out.println("Sum of two numbers "+first+" and "+second+" is = "+sum);
+ 		
+ 		sc.close();
+ 	}
+}
