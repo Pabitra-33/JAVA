387eafad21040f008f48e28fb4371e444bfea285
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-16 17:23:12 +0530

    looping statements in java

---
+++ Basics_Java/src/com/loops/DoWhileLoop.java
@@ -0,0 +1,22 @@
+package com.loops;
+
+import java.util.Scanner;
+
+public class DoWhileLoop {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 		System.out.println("Enter a range: ");
+ 		int range = sc.nextInt();
+
+ 		int i = 1;
+        int sum = 0;
+         
+         do {
+             sum += i;
+             i++;
+         } while (i <= range);
+         
+        System.out.println("Sum of numbers from 1 to "+range+" is: " + sum);
+        sc.close();
+ 	}
+}
