ac8f764a5e93a618400c60393510c1bac18a092c
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-19 23:11:14 +0530

    calculating the sum of the elements which are natural elements

---
+++ Basics_Java/src/com/loopsnumberprograms/SumOfNaturalNumbers.java
@@ -0,0 +1,20 @@
+package com.loopsnumberprograms;
+
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+	public static void main(String[] args) {
+ 		Scanner scn = new Scanner(System.in);
+ 		
+ 		System.out.println("Enter the number: ");
+ 		int num = scn.nextInt();
+ 		
+ 		//to calculate & store sum, this variable taken
+ 		int sum = 0;
+ 		for(int i=1; i<= num; i++) {
+ 			sum += i;//adding each element to sum
+ 		}
+ 		System.out.println("Sum of N natural numbers from 1 to "+num+" is = "+sum);
+ 		scn.close();
+ 	}
+}
