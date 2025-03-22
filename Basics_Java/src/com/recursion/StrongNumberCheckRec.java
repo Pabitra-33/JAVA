3d6841383aaddab207c8da73cc64ddce893189d2
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-22 23:34:09 +0530

    strong number check first commit

---
+++ Basics_Java/src/com/recursion/StrongNumberCheckRec.java
@@ -0,0 +1,31 @@
+package com.recursion;
+
+import java.util.Scanner;
+
+public class StrongNumberCheckRec {
+	public static void main(String[] args) {
+ 		Scanner sc = new Scanner(System.in);
+ 		System.out.println("Enter a number: ");
+ 		int num = sc.nextInt();
+ 		
+ 		int sum = strong(num);
+ 		if(sum == num) {
+ 			System.out.println(num+" is a Strong Number");
+ 		}
+ 		else {
+ 			System.out.println(num+" is not a Strong Number");
+ 		}
+ 		sc.close();
+ 	}
+	
+	//strong check
+	public static int strong(int num) {
+ 		if(num == 0) return 0;
+ 		return fact(num%10) + strong(num/10);
+ 	}
+ 
+ 	public static int fact(int n) {
+ 		if(n == 0) return 1;
+ 		return n*fact(n-1);
+ 	}
+}
