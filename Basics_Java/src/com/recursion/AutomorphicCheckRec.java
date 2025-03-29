23c5660ebabdea522ad7e7bfec77fe8778b907e6
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-29 20:44:31 +0530

    checking a given number is automorphic number or not using method recursion

---
+++ Basics_Java/src/com/recursion/AutomorphicCheckRec.java
@@ -0,0 +1,31 @@
+package com.recursion;
+
+import java.util.Scanner;
+
+public class AutomorphicCheckRec {
+	public static void main(String[] args) {
+ 		Scanner sc = new Scanner(System.in);
+ 		System.out.println("Enter a number: ");
+ 		int n = sc.nextInt();
+ 		int sq = n*n;//square of the number
+ 		
+ 		if(isAutomorphic(n,sq)) {
+ 			System.out.println(n+" is an Automorphic Number");
+ 		}
+ 		else {
+ 			System.out.println(n+" is not an Automorphic Number");
+ 		}
+ 		sc.close();
+ 	}
+ 	
+ 	public static boolean isAutomorphic(int num,int sqr){
+ 		//boolean flag = true;
+ 		while(num > 0) {
+ 			if(num%10 != sqr%10) {
+ 				return false;
+ 			}
+ 			return true;
+ 		}
+ 		return isAutomorphic(num/10, sqr/10);
+ 	}
+}
