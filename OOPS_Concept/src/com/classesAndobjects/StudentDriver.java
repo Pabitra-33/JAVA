aa1d24036e65a301697cc6707a46f23fd36429a8
Pabitra-33 <kumarpabitra345@gmail.com>
2025-04-07 23:33:55 +0530

    Created a student class which contain the student properties and display method to display details

---
+++ OOPS_Concept/src/com/classesAndobjects/StudentDriver.java
@@ -0,0 +1,24 @@
+package com.classesAndobjects;
+
+public class StudentDriver {
+	public static void main(String[] args) {
+ 		Student s1 = new Student();
+ 		s1.insti = "JSP";
+ 		s1.name = "Ramana";
+ 		s1.age = 25;
+ 		s1.id = 1;
+ 		s1.mobile = 9564327691l;
+ 		
+ 		Student s2 = new Student();
+ 		s2.insti = "QSP";
+ 		s2.name = "Kavya";
+ 		s2.age = 23;
+ 		s2.id = 3;
+ 		s2.mobile = 8534327691l;
+ 		
+ 		//displaying data
+ 		s1.display();
+ 		System.out.println("==========");
+ 		s2.display();
+ 	}
+}
