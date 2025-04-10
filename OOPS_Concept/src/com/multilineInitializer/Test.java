d132cdbe90105557b70917bb959b8f128942cb71
Pabitra-33 <kumarpabitra345@gmail.com>
2025-04-10 23:42:00 +0530

    checking the static and non-static block execution in terms of object creation

---
+++ OOPS_Concept/src/com/multilineInitializer/Test.java
@@ -0,0 +1,20 @@
+package com.multilineInitializer;
+
+public class Test {
+	//non-static block
+	{
+		System.out.println("Non-Static Block-1");
+	}
+	 	
+	public static void main(String[] args) {
+		System.out.println("Main Starts");
+	 	Test t1 = new Test();//for each new object created the non-static block will execute.
+	 	Test t2 = new Test();
+	 	System.out.println("Main Ends");
+	 }
+	
+	 //non-Static block
+	 {
+	 	System.out.println("Non-static Block-2");
+	 }
+}
