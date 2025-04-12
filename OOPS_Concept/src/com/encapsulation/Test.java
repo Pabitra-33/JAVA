0b3d4772b4d0ac74ec0bfb2160047bcf56df92f9
Pabitra-33 <kumarpabitra345@gmail.com>
2025-04-12 22:55:22 +0530

    Created a perfect encapsulated class

---
+++ OOPS_Concept/src/com/encapsulation/Test.java
@@ -0,0 +1,39 @@
+package com.encapsulation;
+
+//creating a encapsulated class that contains only the private data members
+//and public member functions to initialize them
+class PureEncapsulatedClass {
+	private int i;//outside this class we can not access
+	private int j;
+	private int k;
+	
+	//getter and setter method
+	public int getI() {
+		return i;
+	}
+	public void setI(int i) {
+		this.i = i;
+	}
+	public int getJ() {
+		return j;
+	}
+	public void setJ(int j) {
+		this.j = j;
+	}
+	public int getK() {
+		return k;
+	}
+	public void setK(int k) {
+		this.k = k;
+	}
+	
+}
+
+//implementation class
+public class Test {
+	public static void main(String[] args) {
+		//creating the object of the encapsulated class to access the properties
+		PureEncapsulatedClass encap = new PureEncapsulatedClass();
+		int val1 = encap.getI();//default value assigned
+	}
+}
