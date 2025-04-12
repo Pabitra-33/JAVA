dbb730314d110bbebc24e882388a3fd11d1a2b62
Pabitra-33 <kumarpabitra345@gmail.com>
2025-04-12 22:59:54 +0530

    created a perfectly encapsulated class that will contains the private data members and the public member functions to access them

--- OOPS_Concept/src/com/encapsulation/Test.java
+++ OOPS_Concept/src/com/encapsulation/Test.java
@@ -35,5 +35,12 @@ public class Test {
 		//creating the object of the encapsulated class to access the properties
 		PureEncapsulatedClass encap = new PureEncapsulatedClass();
 		int val1 = encap.getI();//default value assigned
+		System.out.println(val1);//0
+		encap.setI(10);
+		encap.setJ(30);
+		encap.setK(60);
+		System.out.println("Value of i= "+encap.getI());//10
+		System.out.println("Value of j= "+encap.getJ());//30
+		System.out.println("Value of k= "+encap.getK());//60
 	}
 }
