94b27e71073b5bb80509cc0e111685df092e0efd
Pabitra-33 <kumarpabitra345@gmail.com>
2025-04-12 23:26:27 +0530

    read only classes are basically used for reading the data, we can not modify the data

---
+++ OOPS_Concept/src/com/encapsulation/ReadonlyStudent.java
@@ -0,0 +1,11 @@
+package com.encapsulation;
+
+//A Java class which has only getter methods(read-only class).
+public class ReadonlyStudent {
+	private String college="AKG"; 
+	
+	//getter method for college  
+	public String getCollege() { 
+		return college;   
+	}  
+}
