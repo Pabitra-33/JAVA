4e7ac3b30d238393d2cdf39b3921a8a7e18ddd60
Pabitra-33 <kumarpabitra345@gmail.com>
2025-04-13 00:33:40 +0530

    perfect encapsulated employee class with private members and public functions

--- OOPS_Concept/src/com/encapsulation/Employee.java
+++ OOPS_Concept/src/com/encapsulation/Employee.java
@@ -1,5 +1,41 @@
 package com.encapsulation;
 
+//Class "Employee"
 public class Employee {
+	private String emp_name;
+	private String emp_id;
+	private double net_salary;
+	
+	// Constructor
+	public Employee(String emp_name, String emp_id, double net_salary) {
+		this.emp_name = emp_name;
+	    this.emp_id = emp_id;
+	    this.net_salary = net_salary;
+	}
+	
+	// Getter methods
+	public String getEmpName() {
+	 return emp_name;
+	}
 
+	public String getEmpId() {
+	 return emp_id;
+	}
+
+	public double getSalary() {
+	 return net_salary;
+	}
+	
+	// Setter methods
+	public void setEmpName(String emp_name) {
+	 this.emp_name = emp_name;
+	}
+
+	public void setEmpId(String emp_id) {
+	 this.emp_id = emp_id;
+	}
+
+	public void setSalary(double net_salary) {
+	 this.net_salary = net_salary;
+	}
 }
