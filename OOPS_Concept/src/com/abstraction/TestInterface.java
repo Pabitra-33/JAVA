f41779a151487b3887c4295c607d783071460ad5
Pabitra-33 <kumarpabitra345@gmail.com>
2025-04-15 09:40:03 +0530

    testing the abstraction using interface

---
+++ OOPS_Concept/src/com/abstraction/TestInterface.java
@@ -0,0 +1,21 @@
+package com.abstraction;
+
+interface Int {
+	int i = 10;//by default public static final int i = 10
+	void m1();//by default public abstract void m1()
+}
+
+class A implements Int {
+	@Override
+	public void m1() {
+		System.out.println("m1() in A class");
+	}
+}
+public class TestInterface {
+	public static void main(String[] args) {
+		//Int i = new Int();//CTE
+		Int i1 = new A();
+		i1.m1();//m1() in A class
+		System.out.println("Variable i: "+Int.i);
+	}
+}
