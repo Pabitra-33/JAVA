127343c0c0c9d7174ad36fc5384d1b9ac37be3fb
Pabitra-33 <kumarpabitra345@gmail.com>
2025-04-15 09:09:26 +0530

    achieving the abstraction using the interface

--- OOPS_Concept/src/com/abstraction/DriverShape.java
+++ OOPS_Concept/src/com/abstraction/DriverShape.java
@@ -7,12 +7,13 @@ interface ShapeInter {
 
 //Class implementing the interface
 class Circles implements ShapeInter {
+	@Override
 	public void draw() { // Implementation of the interface method
 		System.out.println("Drawing a circle.");
-    }
+	}
 }
 
-//Testing the implementation
+//Testing the implementation of the abstraction
 public class DriverShape {
 	public static void main(String[] args) {
 		ShapeInter shape = new Circles(); // Polymorphic reference
