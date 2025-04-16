e86adb18fc74d55c00e853ad928454da77b96bfb
Pabitra-33 <kumarpabitra345@gmail.com>
2025-04-16 22:39:49 +0530

    performs the method overriding by changing the implementation

---
+++ OOPS_Concept/src/com/polymorphism/Animal.java
@@ -0,0 +1,21 @@
+package com.polymorphism;
+
+public class Animal {
+	public void makeSound() {
+        System.out.println("Generic animal sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Woof!");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Meow!");
+    }
+}
