dde3cd4f33a8ad96283d7aff4ddf40303d04927a
Pabitra-33 <kumarpabitra345@gmail.com>
2025-04-17 23:52:55 +0530

    method overloading helps us to achieve compile-time polymorphism using different methods

--- OOPS_Concept/src/com/polymorphism/HelperOverloading.java
+++ OOPS_Concept/src/com/polymorphism/HelperOverloading.java
@@ -1,26 +1,21 @@
 package com.polymorphism;
 
 //Method overloading By using, Different Types of Arguments
-//Class 1
-//Helper class
+//Class 1 Helper class
 class Helper {
 	// Method with 2 integer parameters
-    static int Multiply(int a, int b)
-    {
-    	// Returns product of integer numbers
-        return a * b;
+    static int Multiply(int a, int b){
+        return a * b;// Returns product of integer numbers
     }
 
-    // Method 2
-    // With same name but with 2 double parameters
+    // Method 2 With same name but with 2 double parameters
     static double Multiply(double a, double b) {
-    	// Returns product of double numbers
-        return a * b;
- }
+        return a * b;// Returns product of double numbers
+    }
 }
 
 public class HelperOverloading {
-	 //Main driver method
+	//Main driver method
 	public static void main(String[] args) {
 		// Calling method by passing
 	    // input as in arguments
