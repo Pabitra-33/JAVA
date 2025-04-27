4942b852ed30b294cec84c53650f02e60804361b
Pabitra-33 <kumarpabitra345@gmail.com>
2025-04-27 10:42:34 +0530

    created a class and creating it's object and calling the clone(), we are copying the values of first object to second object

--- OOPS_Concept/src/com/cloning/TestCloning.java
+++ OOPS_Concept/src/com/cloning/TestCloning.java
@@ -4,7 +4,7 @@ class Demo implements Cloneable {
 	int i;
 	int j;
 	
-	//constructor
+	//parameterized constructor
 	public Demo(int i, int j) {
 		this.i = i;
 		this.j = j;
@@ -16,7 +16,27 @@ class Demo implements Cloneable {
 		return "Demo [i=" + i + ", j=" + j + "]";
 	}
 	
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
 }
-public class TestCloning {
 
+//implementation class
+public class TestCloning {
+	public static void main(String[] args) {
+		//first object created
+		Demo d1 = new Demo(10, 30);
+		
+		//cloning the second object from the first object
+		Demo d2 = null;
+		try {
+			d2 = (Demo)d1.clone();
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("First Object : "+ d1);
+		System.out.println("Second Object : "+ d2);
+	}
 }
