af376bb4ae4f6ad235393dcd09a18c428e9b4f67
Pabitra-33 <kumarpabitra345@gmail.com>
2025-04-29 10:41:52 +0530

    finding the occurrence of a given character from the given string

--- String_Programs/src/com/stringmethods/IndexOfMethod.java
+++ String_Programs/src/com/stringmethods/IndexOfMethod.java
@@ -2,6 +2,10 @@ package com.stringmethods;
 
 public class IndexOfMethod {
 	public static void main(String[] args) {
-		
+		String s = new String("Welcome to our Lord's Universe");
+
+        System.out.print("Found o first at position: ");
+
+        System.out.println(s.indexOf('o'));//4 in welcome
 	}
 }
