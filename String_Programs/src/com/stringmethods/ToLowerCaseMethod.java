b51c0f2ccf0ea792b1aac2d9594163b942c11c71
Pabitra-33 <kumarpabitra345@gmail.com>
2025-04-29 08:10:58 +0530

    converts the string to lower case using the tolowercase method

--- String_Programs/src/com/stringmethods/ToLowerCaseMethod.java
+++ String_Programs/src/com/stringmethods/ToLowerCaseMethod.java
@@ -3,7 +3,7 @@ package com.stringmethods;
 public class ToLowerCaseMethod {
 	public static void main(String[] args) {
 		String str = "My Name Is PK Sahoo";
-		System.out.println(str);//My Name Is PK Sahoo
+		System.out.println("Normal String: "+ str);//My Name Is PK Sahoo
 		
 		//converting to lower case using methods
 		str = str.toLowerCase();
