e54992bc8c0a7f928b62b7fa7638c00577bef707
Pabitra-33 <kumarpabitra345@gmail.com>
2025-05-06 09:35:17 +0530

    converting the number associated with the strings to character

--- String_Programs/src/com/stringprograms/ConvertNoToChar.java
+++ String_Programs/src/com/stringprograms/ConvertNoToChar.java
@@ -1,4 +1,4 @@
-package com.sreingprograms;
+package com.stringprograms;
 
 public class ConvertNoToChar {
 	public static void main(String[] args) {
@@ -7,7 +7,9 @@ public class ConvertNoToChar {
 		char[] ch = s.toCharArray();
 		String temp = "";
 		
+		//to iterate over character array
 		for(int i=0; i<ch.length; i++) {
+			//skip if the character is a number
 			if(ch[i]>='0' && ch[i]<='9') continue;
 			for(int j=1; j<=(ch[i+1]-'0');j++) {
 				temp += ch[i];
