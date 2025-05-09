e13c79258bb98ec29c6f78de46707e717884402d
Pabitra-33 <kumarpabitra345@gmail.com>
2025-05-09 19:45:10 +0530

    reversing each characters and words of a given string

--- String_Programs/src/com/stringprograms/StringEachCharReverse.java
+++ String_Programs/src/com/stringprograms/StringEachCharReverse.java
@@ -3,14 +3,14 @@ package com.stringprograms;
 public class StringEachCharReverse {
 	public static void main(String[] args) {
 		String s = "java is easy";//given string
-		System.out.println(rev(s));
+		System.out.println(rev(s)); //output: ysae si avaj
 	}
 	
 	public static String rev(String s) {
-		int j= s.length()-1;
+		int j = s.length()-1;
 		String rev="";
-		while(j>=0) {
-			rev+=s.charAt(j);
+		while(j >= 0) {
+			rev += s.charAt(j);
 			j--;
 		}
 		return rev;
