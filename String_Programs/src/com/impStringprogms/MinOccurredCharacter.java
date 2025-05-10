fa45abb71f1d17151490abd1ba7cde2cb6579f70
Pabitra-33 <kumarpabitra345@gmail.com>
2025-05-10 22:50:34 +0530

    printing the minimum occurred character in a given string

---
+++ String_Programs/src/com/impStringprogms/MinOccurredCharacter.java
@@ -0,0 +1,32 @@
+package com.impStringprogms;
+
+import java.util.Scanner;
+
+public class MinOccurredCharacter {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a String:");
+		String s = sc.nextLine();
+		
+		char[] ch = s.toCharArray();//converting to character array
+		int minCount = s.length();
+		char minChar = '\u0000'; //or: ' '- empty character
+		
+		for(int i=0; i<ch.length; i++) {
+			int count = 1;
+			if(ch[i] == '\u0000') continue;//skip it
+			for(int j=i+1; j<ch.length; j++) {
+				if(ch[i] == ch[j]) {
+					count++;
+				    ch[j] = '\u0000';
+			    }
+			}
+			if(count < minCount) {
+				minCount = count;
+				minChar = ch[i];
+			}
+		}
+		 System.out.println("Minimum Ocurred character in the String is: "+minChar+" = "+minCount);	
+		 sc.close();	
+	}
+}
