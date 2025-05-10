00ec6c7ba026ac8ba7f220f6af116183abf8ca50
Pabitra-33 <kumarpabitra345@gmail.com>
2025-05-10 22:50:59 +0530

    printing the maximum occurred character in a given string

---
+++ String_Programs/src/com/impStringprogms/MaxOccurredCharacter.java
@@ -0,0 +1,32 @@
+package com.impStringprogms;
+
+import java.util.Scanner;
+
+public class MaxOccurredCharacter {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a String:");
+		String s = sc.nextLine();
+		
+		char[] ch = s.toCharArray();//converting to character array
+		int maxCount = 0;
+		char maxChar = '\u0000'; //or: ' '- empty character
+		
+		for(int i=0; i<ch.length; i++) {
+			int count = 1;
+			if(ch[i] == '\u0000') continue;//skip it
+			for(int j=i+1; j<ch.length; j++) {
+				if(ch[i] == ch[j]) {
+					count++;
+				    ch[j] = '\u0000';
+			    }
+			}
+			if(count > maxCount) {
+				maxCount = count;
+				maxChar = ch[i];
+			}
+		}
+		 System.out.println("Maximum Ocurred character in the String is: "+maxChar+" = "+maxCount);	
+		 sc.close();	
+	}
+}
