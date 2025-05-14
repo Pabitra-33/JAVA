17437ce44404af2240b02a5f732a278a7eaf809c
Pabitra-33 <kumarpabitra345@gmail.com>
2025-05-14 23:33:05 +0530

    creating array in a java

---
+++ String_Programs/src/main/arrayDemo/Test.java
@@ -0,0 +1,10 @@
+package main.arrayDemo;
+
+public class Test {
+	public static void main(String[] args) {
+		int[] arr = {10,20,30,40};
+		for (int i : arr) {
+			System.out.println(i+" ");
+		}
+	}
+}
