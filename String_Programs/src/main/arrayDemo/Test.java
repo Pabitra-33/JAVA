f74ad84c6eea66a5140eba3856ecc5d6778513a0
Pabitra-33 <kumarpabitra345@gmail.com>
2025-05-14 23:49:55 +0530

    changed the code by adding a new method and call that method to increase my array elements

--- String_Programs/src/main/arrayDemo/Test.java
+++ String_Programs/src/main/arrayDemo/Test.java
@@ -4,7 +4,19 @@ public class Test {
 	public static void main(String[] args) {
 		int[] arr = {10,20,30,40};
 		for (int i : arr) {
-			System.out.println(i+" ");
+			System.out.print(i+" ");
+		}
+		System.out.println();//new line
+		m1(arr);//calling the method
+		for (int i : arr) {
+			System.out.print(i+" ");
+		}
+	}
+	
+	//static method
+	static void m1(int[] a) {
+		for (int i = 0; i < a.length; i++) {
+			a[i]++;//increasing array each elements
 		}
 	}
 }
