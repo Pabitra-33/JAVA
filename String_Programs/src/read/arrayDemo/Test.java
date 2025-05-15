7b3e510b04e7c41445353450a9fc68695efd6fa8
Pabitra-33 <kumarpabitra345@gmail.com>
2025-05-15 23:30:07 +0530

    creation of an array in java and assigning value to it

--- String_Programs/src/read/arrayDemo/Test.java
+++ String_Programs/src/read/arrayDemo/Test.java
@@ -1,4 +1,4 @@
-package main.arrayDemo;
+package read.arrayDemo;
 
 public class Test {
 	public static void main(String[] args) {
--- String_Programs/src/read/arrayDemo/Test2.java
+++ String_Programs/src/read/arrayDemo/Test2.java
@@ -1,4 +1,4 @@
-package main.arrayDemo;
+package read.arrayDemo;
 
 import java.util.Arrays;
 
