151353cff5421422d99a0ced0e323f6dfd45f57a
Pabitra-33 <kumarpabitra345@gmail.com>
2025-05-18 13:01:53 +0530

    changing the code logic and written it in a method and called it

--- String_Programs/src/read/arrayprograms/SumOfAntiDigonalElements.java
+++ String_Programs/src/read/arrayprograms/SumOfAntiDigonalElements.java
@@ -2,7 +2,11 @@ package read.arrayprograms;
 
 public class SumOfAntiDigonalElements {
 	public static void main(String[] args) {
-		int[][] arr = {{1,2,3},{7,8,9},{13,14,15}};
+		int[][] a = {{1,2,3},{7,8,9},{13,14,15}};
+		System.out.println(sumOfAntiDigonals(a));//40 (sum of anti-diagonals: 1+8+15+3+11)
+	}
+	
+	public static int sumOfAntiDigonals(int[][] arr) {
 		int len = arr.length;//finding the length of the array
 		int sum = 0; //to store the sum
 		
@@ -16,6 +20,6 @@ public class SumOfAntiDigonalElements {
 			
 			sum += arr[i][len-1-i];
 		}
-		System.out.println("Sum is: "+sum);//40 (sum of anti-diagonals: 1+8+15+3+11)
+		return sum;
 	}
 }
