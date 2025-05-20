42fd277812e7dceb0e86dbbb273d954fa618fd93
Pabitra-33 <kumarpabitra345@gmail.com>
2025-05-20 09:51:47 +0530

    performed another regular expression program

---
+++ Regular_Expressions/src/com/demoExpressions/RegularExpressionTest.java
@@ -0,0 +1,15 @@
+package com.demoExpressions;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegularExpressionTest {
+	public static void main(String[] args) {
+		//finding the pattern in the String
+		Pattern p=Pattern.compile("x"); 
+		Matcher m=p.matcher("a1b7@z#"); 
+		while(m.find()) { 
+			  System.out.println(m.start()+"------"+m.group()); 
+		 }
+	}
+}
