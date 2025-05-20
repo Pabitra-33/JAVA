4d3e2e45073afc7e26d0251eec495a617267e761
Pabitra-33 <kumarpabitra345@gmail.com>
2025-05-20 09:48:00 +0530

    created a demo regex class to find out a demo pattern using pattern and matcher class

--- Regular_Expressions/src/com/demoExpressions/RegularExpressionDemo.java
+++ Regular_Expressions/src/com/demoExpressions/RegularExpressionDemo.java
@@ -1,5 +1,20 @@
 package com.demoExpressions;
 
-public class RegularExpressionDemo {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class RegularExpressionDemo {
+	public static void main(String[] args) { 
+	  int count=0; 
+	  //pattern class
+	  Pattern p=Pattern.compile("ab"); 
+	  //matcher class
+	  Matcher m=p.matcher("abbbabbaba"); 
+	  while(m.find()) //finding the matched pattern
+	  { 
+	       count++; 
+	       System.out.println(m.start()+"------"+m.end()+"------"+m.group()); 
+	  } 
+	  System.out.println("The no of occurences :"+count); 
+	 }
 }
