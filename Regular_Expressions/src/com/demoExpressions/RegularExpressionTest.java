9e5e383ce247b17445c385f298a5324cc8bb8242
Pabitra-33 <kumarpabitra345@gmail.com>
2025-05-20 10:16:46 +0530

    testing the string by changing it's content to find the pattern expressions

--- Regular_Expressions/src/com/demoExpressions/RegularExpressionTest.java
+++ Regular_Expressions/src/com/demoExpressions/RegularExpressionTest.java
@@ -7,7 +7,7 @@ public class RegularExpressionTest {
 	public static void main(String[] args) {
 		//finding the pattern in the String
 		Pattern p=Pattern.compile("x"); 
-		Matcher m=p.matcher("a1b7 x@z#"); 
+		Matcher m=p.matcher("a1xb7 x@z#"); 
 		while(m.find()) { 
 			  System.out.println(m.start()+"------"+m.group()); 
 		 }
