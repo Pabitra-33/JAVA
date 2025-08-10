3cf8a2528a2030c04229e93a2c01f355c2d0086b
Pabitra-33 <kumarpabitra345@gmail.com>
2025-08-10 18:15:36 +0530

    testing completed of all the methods and it works fine

--- DataStructurePrograms/src/com/LinearDS/TestArrayList.java
+++ DataStructurePrograms/src/com/LinearDS/TestArrayList.java
@@ -5,5 +5,27 @@ public class TestArrayList {
 		ArrayListImplementation implementation = new ArrayListImplementation();
 		
 		//add elements
+		implementation.add(10);
+		implementation.add(20);
+		implementation.add(30);
+		implementation.add(50);
+		implementation.add(60);
+		implementation.add(70);
+		
+		//printing the size
+		System.out.println("Size of the Arraylist is: "+implementation.size());
+		
+		System.out.println("Elements are: ");
+		for(int i=0; i<implementation.size(); i++) {
+			System.out.print((Integer) implementation.get(i)+" ");
+		}
+		
+		implementation.remove(3);
+		System.out.println();
+		
+		System.out.println("After reomval elements are: ");
+		for(int i=0; i<implementation.size(); i++) {
+			System.out.print((Integer) implementation.get(i)+" ");
+		}
 	}
 }
