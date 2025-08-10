b0291d58f5ebda30a0c4efe9abe6105cce9cd686
Pabitra-33 <kumarpabitra345@gmail.com>
2025-08-10 17:43:13 +0530

    to test my application i have created a testarraylist class

---
+++ DataStructurePrograms/src/com/LinearDS/TestArrayList.java
@@ -0,0 +1,9 @@
+package com.LinearDS;
+
+public class TestArrayList {
+	public static void main(String[] args) {
+		ArrayListImplementation implementation = new ArrayListImplementation();
+		
+		//add elements
+	}
+}
