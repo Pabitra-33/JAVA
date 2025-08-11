a45a9a415c78bd34e5337d8cc16fc30931a80d67
Pabitra-33 <kumarpabitra345@gmail.com>
2025-08-11 10:28:20 +0530

    created a add method in linked list to store new nodes

---
+++ DataStructurePrograms/src/com/LinearDS/LinkedListImplementation.java
@@ -0,0 +1,26 @@
+package com.LinearDS;
+
+//to provide implementation taken a implementation class
+public class LinkedListImplementation {
+	private Node first = null;//aggregation
+	private int count = 0;//for node counting purpose
+	
+	//adding a node
+	public void add(Object ele) {
+		//if the first is null then create a new node and assign to it
+		if(first == null) {
+			first = new Node(ele, null);
+			count++;//increasing count as we added a node
+			return;
+		}
+		
+		//if the first is not null, then traverse till the end and add a new node
+		Node curr = first;//taking a temporary variable to traverse till end and add
+		while(curr.next != null) {
+			curr = curr.next;//by this end of the loop we will reach the end.
+		}
+		
+		curr.next = new Node(ele, null);//storing the new node in the curr.next position to make a chain
+		count++;
+	}
+}
