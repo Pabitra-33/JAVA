5b6780d7d05dd6d5350ec4fd5bec34bd14e7250a
Pabitra-33 <kumarpabitra345@gmail.com>
2025-11-02 20:28:05 +0530

    moving the zeroes to the front and ones to the end

---
+++ String_Programs/src/com/ArrayInterviewPrograms/ZeroesOnes.java
@@ -0,0 +1,29 @@
+package com.ArrayInterviewPrograms;
+
+import java.util.Arrays;
+
+//program to move zeros to first and ones to last.
+
+public class ZeroesOnes {
+    public static void main(String[] args) {
+        int[] arr = {1,1,0,1,0,1,0,0};
+        System.out.println("Before Sorting: " + Arrays.toString(arr));
+
+        int i = 0;
+        int j = arr.length - 1;
+
+        while (i < j) {
+            while (arr[i] == 0 && i < j) i++;
+            while (arr[j] == 1 && i < j) j--;
+            if (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        System.out.println("After Sorting: " + Arrays.toString(arr));
+    }
+}
