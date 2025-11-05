48166784935b5644ae6d0f9815a78e260d6805b1
Pabitra-33 <kumarpabitra345@gmail.com>
2025-11-05 08:16:37 +0530

    performing the String compression for a given input of the character string

---
+++ String_Programs/src/com/StringInterviewPrograms/StringCompressor.java
@@ -0,0 +1,34 @@
+package com.StringInterviewPrograms;
+
+public class StringCompressor {
+	public static void main(String[] args) {
+		String str = "aaabbcdd";
+		System.out.println(compressString(str));
+	}
+
+	// string compress method
+	public static String compressString(String s) {
+		// first checking whether the String is null or empty
+		if (s == null || s.isEmpty()) {
+			return "";
+		}
+
+		StringBuilder result = new StringBuilder();// to store result
+		int count = 1;
+
+		// traversing the string and performing string compression
+		for (int i = 1; i < s.length(); i++) {
+			if (s.charAt(i) == s.charAt(i - 1)) {
+				count++;
+			} else {
+				result.append(s.charAt(i - 1)).append(count);
+				count = 1;
+			}
+		}
+
+		// Append last character and its count
+		result.append(s.charAt(s.length() - 1)).append(count);
+
+		return result.toString();
+	}
+}
