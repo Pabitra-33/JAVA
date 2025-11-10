00628d6f8779e6f31a0ab55f339dc537005ba4ee
Pabitra-33 <kumarpabitra345@gmail.com>
2025-11-10 23:30:36 +0530

    replacing the vowels with the next consonants

---
+++ String_Programs/src/com/StringInterviewPrograms/ReplaceVowelWithNextConsonants.java
@@ -0,0 +1,29 @@
+package com.StringInterviewPrograms;
+
+public class ReplaceVowelWithNextConsonants {
+	public static void main(String[] args) {
+		//Given String
+		String str = "Java Is Fun";
+		String[] words = str.split(" ");
+		
+		StringBuilder result = new StringBuilder();
+		
+		//take each word from the sentence
+		for(String word : words) {
+			char[] chars = word.toCharArray();
+			
+			for(int i=0; i<chars.length; i++) {
+				char ch=chars[i];
+				
+				if("aeiou".contains(ch+"")) {
+					if(chars[i+1]=='a'|| chars[i+1]=='e'|| chars[i+1] =='i'|| chars[i+1]=='o'|| chars[i+1]=='u') {
+						continue;
+					}
+					chars[i] = chars[i+1];
+				}
+				result.append(chars[i]);
+			}
+		}
+		System.out.println(result);
+	}
+}
