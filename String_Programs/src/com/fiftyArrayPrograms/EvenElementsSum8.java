1b6e205d69e30ce95f031bfdca24a5cd4f2b8711
Pabitra-33 <kumarpabitra345@gmail.com>
2025-11-12 16:30:50 +0530

    printing the even elements sum in the array

---
+++ String_Programs/src/com/fiftyArrayPrograms/EvenElementsSum8.java
@@ -0,0 +1,22 @@
+package com.fiftyArrayPrograms;
+
+public class EvenElementsSum8 {
+	public static void main(String[] args) {
+		// Declaring and initializing the array
+		int[] numbers = { 1, 2, 3, 4, 5, 6 };
+
+		// Initializing a variable to store the sum of even elements
+		int evenSum = 0;
+
+		// Looping through each element of the array
+		for (int i = 0; i < numbers.length; i++) {
+			// Checking if the element is even
+			if (numbers[i] % 2 == 0) {
+				evenSum += numbers[i]; // Add even element to sum
+			}
+		}
+
+		// Printing the result
+		System.out.println("Sum of even elements in the array: " + evenSum);
+	}
+}
