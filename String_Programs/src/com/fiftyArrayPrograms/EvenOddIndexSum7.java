5dfde4d11cb75ba00c97db3b7839341cfae47847
Pabitra-33 <kumarpabitra345@gmail.com>
2025-11-12 16:30:28 +0530

    printing the even and odd index elements in the array

---
+++ String_Programs/src/com/fiftyArrayPrograms/EvenOddIndexSum7.java
@@ -0,0 +1,25 @@
+package com.fiftyArrayPrograms;
+
+public class EvenOddIndexSum7 {
+	public static void main(String[] args) {
+		// Declaring and initializing the array
+		int[] numbers = { 1, 2, 3, 4, 5, 6 };
+
+		// Initializing variables to store sums
+		int evenIndexSum = 0;
+		int oddIndexSum = 0;
+
+		// Looping through the array
+		for (int i = 0; i < numbers.length; i++) {
+			if (i % 2 == 0) {
+				evenIndexSum += numbers[i]; // Adding elements at even index
+			} else {
+				oddIndexSum += numbers[i]; // Adding elements at odd index
+			}
+		}
+
+		// Printing the results
+		System.out.println("Sum of even index values: " + evenIndexSum);
+		System.out.println("Sum of odd index values: " + oddIndexSum);
+	}
+}
