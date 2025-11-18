d7dd3163eee282715cd08017cc82742924f3ca27
Pabitra-33 <kumarpabitra345@gmail.com>
2025-11-18 21:55:04 +0530

    printing the last repeated updated element in the array

---
+++ String_Programs/src/com/fiftyArrayPrograms/LastIndexValue15.java
@@ -0,0 +1,20 @@
+package com.fiftyArrayPrograms;
+
+public class LastIndexValue15 {
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 2, 9, 3, 9, 4, 9, 6, 7, 8 };
+		int target = 9;
+
+		int lastIndex = -1; // default (if element not found)
+
+		// Loop through the array
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == target) {
+				lastIndex = i; // update every time a match is found
+			}
+		}
+
+		System.out.println("Last index of " + target + " is: " + lastIndex);
+	}
+}
