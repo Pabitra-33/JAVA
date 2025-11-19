93e36aaba6ae78fe7d36dc21ebd5609eea015b4f
Pabitra-33 <kumarpabitra345@gmail.com>
2025-11-19 22:10:49 +0530

    updated the comment

--- String_Programs/src/com/fiftyArrayPrograms/LastIndexValue15.java
+++ String_Programs/src/com/fiftyArrayPrograms/LastIndexValue15.java
@@ -6,7 +6,7 @@ public class LastIndexValue15 {
 		int[] arr = { 1, 2, 9, 3, 9, 4, 9, 6, 7, 8 };
 		int target = 9;
 
-		int lastIndex = -1; // default (if element not found)
+		int lastIndex = -1; // default (if element not found in the array)
 
 		// Loop through the array
 		for (int i = 0; i < arr.length; i++) {
