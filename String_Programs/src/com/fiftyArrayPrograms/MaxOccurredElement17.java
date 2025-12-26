bf3bab83b23e42b56e16d21e335b01ca0c022cc3
Pabitra-33 <kumarpabitra345@gmail.com>
2025-12-26 21:47:07 +0530

    finding the maximum occurred element in the given array

---
+++ String_Programs/src/com/fiftyArrayPrograms/MaxOccurredElement17.java
@@ -0,0 +1,39 @@
+package com.fiftyArrayPrograms;
+
+public class MaxOccurredElement17 {
+	public static void main(String[] args) {
+
+        int[] arr = {1, 2, 1, 3, 1, 4};
+        boolean[] visited = new boolean[arr.length];
+
+        int maxCount = 0;
+        int maxElement = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+
+            // Skip already counted elements
+            if (visited[i]) {
+                continue;
+            }
+
+            int count = 1;
+
+            // Count occurrences of arr[i]
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    visited[j] = true;
+                }
+            }
+
+            // Update maximum occurred element
+            if (count > maxCount) {
+                maxCount = count;
+                maxElement = arr[i];
+            }
+        }
+
+        System.out.println("Maximum occurred element: " 
+                           + maxElement + " - " + maxCount);
+    }
+}
