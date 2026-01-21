10e7c378bc6d8b4fd9c76b2aeb7cdeb5c7e222b5
Pabitra-33 <kumarpabitra345@gmail.com>
2026-01-21 22:20:26 +0530

    added some comments for better understanding

--- String_Programs/src/com/fiftyArrayPrograms/SecondSmallest23.java
+++ String_Programs/src/com/fiftyArrayPrograms/SecondSmallest23.java
@@ -3,11 +3,13 @@ package com.fiftyArrayPrograms;
 public class SecondSmallest23 {
     public static void main(String[] args) {
 
-        int[] arr = {1, 2, 1, 3, 2, 4};
+        int[] arr = {1, 2, 1, 3, 2, 4};//given array
 
+        //by default making the first and second smallest as MaxValue
         int smallest = Integer.MAX_VALUE;
         int secondSmallest = Integer.MAX_VALUE;
 
+        //iterating the array usin gloop
         for (int i = 0; i < arr.length; i++) {
 
             if (arr[i] < smallest) {
