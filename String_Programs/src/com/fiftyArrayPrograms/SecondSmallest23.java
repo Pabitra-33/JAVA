f1ab4826c03d12d31d581bdbf2fcf29e6ffe84de
Pabitra-33 <kumarpabitra345@gmail.com>
2026-01-21 22:27:09 +0530

    finding the second smallest element in the given array using loop and traversing the array

--- String_Programs/src/com/fiftyArrayPrograms/SecondSmallest23.java
+++ String_Programs/src/com/fiftyArrayPrograms/SecondSmallest23.java
@@ -9,18 +9,19 @@ public class SecondSmallest23 {
         int smallest = Integer.MAX_VALUE;
         int secondSmallest = Integer.MAX_VALUE;
 
-        //iterating the array usin gloop
+        //iterating the array using loop
         for (int i = 0; i < arr.length; i++) {
 
+        	//checking the array element one by one with the smallest element i have assigned
             if (arr[i] < smallest) {
-                secondSmallest = smallest;
+                secondSmallest = smallest;//if it lesser than the smallest then updating the smallest element
                 smallest = arr[i];
             }
             else if (arr[i] < secondSmallest && arr[i] != smallest) {
                 secondSmallest = arr[i];
             }
         }
-
+        //printing the second smallest element from the array in the console
         System.out.println("Second smallest element: " + secondSmallest);
     }
 }
