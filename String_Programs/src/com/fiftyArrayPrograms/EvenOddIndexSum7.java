943e76418d8bac03c3246da88258395b8221898c
Pabitra-33 <kumarpabitra345@gmail.com>
2026-02-01 18:13:57 +0530

    odd and even sum printed

--- String_Programs/src/com/fiftyArrayPrograms/EvenOddIndexSum7.java
+++ String_Programs/src/com/fiftyArrayPrograms/EvenOddIndexSum7.java
@@ -20,7 +20,7 @@ public class EvenOddIndexSum7 {
 		}
 
 		// Printing the results
-		System.out.println("Sum of even index values: " + evenIndexSum);
-		System.out.println("Sum of odd index values: " + oddIndexSum);
+		System.out.println("Sum of even index values: " + evenIndexSum);//even
+		System.out.println("Sum of odd index values: " + oddIndexSum);//odd
 	}
 }
